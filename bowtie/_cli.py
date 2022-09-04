2f8b66d0158c82d6c9a6754bd704241ec836fa69
Julian Berman <Julian@GrayVines.com>
2022-09-04 15:49:28 +0300

    Retry a few times when getting empty responses.

--- bowtie/_cli.py
+++ bowtie/_cli.py
@@ -1,26 +1,24 @@
 from collections import defaultdict
-from contextlib import AsyncExitStack, asynccontextmanager
-from time import monotonic_ns
+from contextlib import AsyncExitStack
 import asyncio
 import json
 import sys
 
-from attrs import define, field
 import aiodocker
 import click
 import structlog
 
+from bowtie._core import Implementation
+
 log = structlog.get_logger()
-
-
-class StartError(Exception):
-    pass
 
 
 @click.group(context_settings=dict(help_option_names=["--help", "-h"]))
 @click.version_option(prog_name="bowtie")
 def main():
-    pass
+    """
+    A meta-validator for the JSON Schema specifications.
+    """
 
 
 @main.command()
@@ -38,93 +36,26 @@ def run(**kwargs):
     asyncio.run(_run(**kwargs, cases=cases))
 
 
-@define(hash=True)
-class Implementation:
-    """
-    A running implementation under test.
-    """
-
-    _name: str
-    _stream: aiodocker.stream.Stream = field(repr=False)
-
-    @classmethod
-    @asynccontextmanager
-    async def start(cls, docker, image_name):
-        container = await docker.containers.create(
-            config=dict(Image=image_name, OpenStdin=True),
-        )
-        try:
-            await container.start()
-            stream = container.attach(stdin=True, stdout=True, stderr=True)
-            self = cls(name=image_name, stream=stream)
-            await self._start()
-            yield self
-            await self._stop()
-        finally:
-            await container.delete(force=True)
-
-    async def _start(self):
-        response = await self._send(cmd="start", version=1)
-        if not response["response"].get("ready"):
-            raise StartError(f"{self._name} is not ready!")
-        elif response["response"].get("version") != 1:
-            raise StartError(f"{self._name} did not speak version 1!")
-
-    async def run_case(self, seq, case):
-        return await self._send(cmd="run", seq=seq, case=case)
-
-    async def _stop(self):
-        await self._send(cmd="stop")
-
-    async def _send(self, **kwargs):
-        started = monotonic_ns()
-        cmd = f"{json.dumps(kwargs)}\n"
-        await self._stream.write_in(cmd.encode("utf-8"))
-
-        message = await self._stream.read_out()
-        if message is None:
-            succeeded, response = None, {}
-        elif message.stream == 2:
-            succeeded, data = False, [message.data]
-            while message.stream == 2:
-                message = await self._stream.read_out()
-                if message is None:
-                    break
-                data.append(message.data)
-            response = {"stderr": b"".join(data), "implementation": self._name}
-        else:
-            succeeded, response = True, json.loads(message.data)
-
-        return {
-            "succeeded": succeeded,
-            "took_ns": monotonic_ns() - started,
-            "response": response,
-            "implementation": self._name,
-        }
-
-
 async def _run(implementations, cases):
     async with AsyncExitStack() as stack:
-        log.msg("Starting", implementations=implementations)
+        log.debug("Starting", implementations=implementations)
         docker = await stack.enter_async_context(aiodocker.Docker())
         streams = [
             await stack.enter_async_context(
                 Implementation.start(docker=docker, image_name=each),
             ) for each in implementations
         ]
-        log.msg("Ready", implementations=streams)
+        log.debug("Ready", implementations=streams)
 
-        for seq, case in enumerate(cases):
-            log.msg("Running", case=case["description"])
-            responses = await run_case(streams=streams, seq=seq, case=case)
+        for seq, case in enumerate(cases, 1):
+            log.debug("Running", seq=seq, case=case["description"])
+            responses = await asyncio.gather(
+                *(each.run_case(seq=seq, case=case) for each in streams),
+            )
             tests = defaultdict(lambda: defaultdict(list))
             for each in responses:
                 if not each["succeeded"]:
-                    log.msg(
-                        "ERROR",
-                        stderr=each.get("stderr"),
-                        implementation=each["implementation"],
-                    )
+                    log.error("ERROR", **each)
                     continue
 
                 results = each["response"]["tests"]
@@ -133,19 +64,14 @@ async def _run(implementations, cases):
                         each["implementation"],
                     )
 
-            result = {
-                "description": case["description"],
-                "schema": case["schema"],
-                "tests": {
-                    k: dict(v) if len(v) > 1 else next(iter(v))
-                    for k, v in tests.items()
-                },
+            results = {
+                k: dict(v) if len(v) > 1 else next(iter(v))
+                for k, v in tests.items()
             }
-            log.msg("Responded", result=result)
-
-        responses = await run_case(streams=streams, seq=seq, case=case)
-        log.msg("Last", responses=responses)
-
-
-async def run_case(streams, **kwargs):
-    return await asyncio.gather(*(each.run_case(**kwargs) for each in streams))
+            log.msg(
+                "Responded",
+                seq=seq,
+                description=case["description"],
+                results=results,
+            )
+        log.msg("Finished", count=seq)
---
+++ bowtie/_core.py
@@ -0,0 +1,84 @@
+from contextlib import asynccontextmanager
+from time import monotonic_ns
+import asyncio
+import json
+
+from attrs import define, field
+import aiodocker
+
+
+class StartError(Exception):
+    """
+    An implementation failed to start properly.
+    """
+
+
+@define(hash=True)
+class Implementation:
+    """
+    A running implementation under test.
+    """
+
+    _name: str
+    _stream: aiodocker.stream.Stream = field(repr=False)
+
+    @classmethod
+    @asynccontextmanager
+    async def start(cls, docker, image_name):
+        container = await docker.containers.create(
+            config=dict(Image=image_name, OpenStdin=True),
+        )
+        try:
+            await container.start()
+            stream = container.attach(stdin=True, stdout=True, stderr=True)
+            self = cls(name=image_name, stream=stream)
+            yield await self._start()
+            await self._stop()
+        finally:
+            await container.delete(force=True)
+
+    async def _start(self):
+        response = await self._send(cmd="start", version=1)
+        if not response["response"].get("ready"):
+            raise StartError(f"{self._name} is not ready!")
+        elif response["response"].get("version") != 1:
+            raise StartError(f"{self._name} did not speak version 1!")
+        return self
+
+    async def run_case(self, seq, case):
+        return await self._send(cmd="run", seq=seq, case=case)
+
+    async def _stop(self):
+        await self._send(cmd="stop")
+
+    async def _send(self, **kwargs):
+        started = monotonic_ns()
+        cmd = f"{json.dumps(kwargs)}\n"
+        await self._stream.write_in(cmd.encode("utf-8"))
+        succeeded, response = await self._recv()
+        return {
+            "succeeded": succeeded,
+            "took_ns": monotonic_ns() - started,
+            "response": response,
+            "implementation": self._name,
+        }
+
+    async def _recv(self, retry=3):
+        for _ in range(retry):
+            message = await self._stream.read_out()
+            if message is None:
+                await asyncio.sleep(0)
+                continue
+
+            if message.stream == 2:
+                data = []
+                while message is not None and message.stream == 2:
+                    data.append(message.data)
+                    message = await self._stream.read_out()
+                return False, {
+                    "stderr": b"".join(data),
+                    "implementation": self._name,
+                }
+            else:
+                return True, json.loads(message.data)
+        return False, {}
