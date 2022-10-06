52bda94f8065c0c3a08822d85fad328970cd6cd8
Julian Berman <Julian@GrayVines.com>
2022-10-06 17:24:25 +0100

    Re-enable AOT compilation for clojure-json-schema.
    Random copy pasting, what could go wrong.
    
    Closes: #47

--- implementations/clojure-json-schema/project.clj
+++ implementations/clojure-json-schema/project.clj
@@ -2,5 +2,5 @@
   :dependencies [[org.clojure/clojure "1.11.0"]
                  [org.clojure/data.json "2.4.0"]
                  [luposlip/json-schema "0.3.4"]]
-  :main ^:skip-aot bowtie-json-schema.core
+  :main bowtie-json-schema.core
   :profiles {:uberjar {:aot :all}})
