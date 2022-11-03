f14512fd85cfbe928a88f0b336fa4e2367a92eea
Julian Berman <Julian@GrayVines.com>
2022-11-03 14:54:03 -0400

    Autoformat C++ implementations (including valijson)

--- implementations/cpp-valijson/bowtie_valijson.cpp
+++ implementations/cpp-valijson/bowtie_valijson.cpp
@@ -14,10 +14,9 @@
 using namespace rapidjson;
 using namespace std;
 using placeholders::_1;
-using valijson::adapters::RapidJsonAdapter;
 using valijson::Schema;
 using valijson::SchemaParser;
-
+using valijson::adapters::RapidJsonAdapter;
 
 const unordered_map<string, SchemaParser::Version> DIALECTS{
     {"http://json-schema.org/draft-07/schema#", SchemaParser::kDraft7},
@@ -25,121 +24,116 @@ const unordered_map<string, SchemaParser::Version> DIALECTS{
     {"http://json-schema.org/draft-03/schema#", SchemaParser::kDraft3},
 };
 
-
 class Registry {
-    const Value* contents;
-    public:
-        Registry(const Value* registryContents) { contents = registryContents; }
+  const Value *contents;
 
-        const Document * fetchDocument(const string &uri)
-        {
-            Document *fetchedRoot = new Document();
-            auto registry = contents->GetObject();
-            fetchedRoot->CopyFrom(registry[uri.c_str()], fetchedRoot->GetAllocator());
-            return fetchedRoot;
-        }
+public:
+  Registry(const Value *registryContents) { contents = registryContents; }
+
+  const Document *fetchDocument(const string &uri) {
+    Document *fetchedRoot = new Document();
+    auto registry = contents->GetObject();
+    fetchedRoot->CopyFrom(registry[uri.c_str()], fetchedRoot->GetAllocator());
+    return fetchedRoot;
+  }
 };
 
-
-void freeDocument(const Document *adapter) {
-    delete adapter;
-}
+void freeDocument(const Document *adapter) { delete adapter; }
 
 int main() {
 
-    string dialect;
-    bool started = false;
+  string dialect;
+  bool started = false;
 
-    for (string line; getline(cin, line);) {
-        Document request;
+  for (string line; getline(cin, line);) {
+    Document request;
 
-        Value response(kObjectType);
-        Document::AllocatorType& allocator = request.GetAllocator();
+    Value response(kObjectType);
+    Document::AllocatorType &allocator = request.GetAllocator();
 
-        request.Parse(line.c_str());
-        string cmd = request["cmd"].GetString();
+    request.Parse(line.c_str());
+    string cmd = request["cmd"].GetString();
 
-        if (cmd == "start") {
-            int version = request["version"].GetInt();
-            assert (version == 1);
+    if (cmd == "start") {
+      int version = request["version"].GetInt();
+      assert(version == 1);
 
-            started = true;
+      started = true;
 
-            response.AddMember("ready", true, allocator);
-            response.AddMember("version", 1, allocator);
+      response.AddMember("ready", true, allocator);
+      response.AddMember("version", 1, allocator);
 
-            Value implementation(kObjectType);
-            implementation.AddMember("language", "c++", allocator);
-            implementation.AddMember("name", "valijson", allocator);
-            implementation.AddMember(
-                    "homepage",
-                    "https://github.com/tristanpenman/valijson",
-                    allocator
-            );
-            implementation.AddMember(
-                    "issues",
-                    "https://github.com/tristanpenman/valijson/issues",
-                    allocator
-            );
+      Value implementation(kObjectType);
+      implementation.AddMember("language", "c++", allocator);
+      implementation.AddMember("name", "valijson", allocator);
+      implementation.AddMember(
+          "homepage", "https://github.com/tristanpenman/valijson", allocator);
+      implementation.AddMember(
+          "issues", "https://github.com/tristanpenman/valijson/issues",
+          allocator);
 
-            Value dialects(kArrayType);
-            dialects.PushBack("http://json-schema.org/draft-07/schema#", allocator);
-            dialects.PushBack("http://json-schema.org/draft-04/schema#", allocator);
-            dialects.PushBack("http://json-schema.org/draft-03/schema#", allocator);
-            implementation.AddMember("dialects", dialects, allocator);
+      Value dialects(kArrayType);
+      dialects.PushBack("http://json-schema.org/draft-07/schema#", allocator);
+      dialects.PushBack("http://json-schema.org/draft-04/schema#", allocator);
+      dialects.PushBack("http://json-schema.org/draft-03/schema#", allocator);
+      implementation.AddMember("dialects", dialects, allocator);
 
-            response.AddMember("implementation", implementation, allocator);
-        } else if (cmd == "dialect") {
-            if (!started) { throw runtime_error("Bowtie hasn't started!"); }
+      response.AddMember("implementation", implementation, allocator);
+    } else if (cmd == "dialect") {
+      if (!started) {
+        throw runtime_error("Bowtie hasn't started!");
+      }
 
-            dialect = request["dialect"].GetString();
-            // TODO: Check me
-            response.AddMember("ok", true, allocator);
-        } else if (cmd == "run") {
-            if (!started) { throw runtime_error("Bowtie hasn't started!"); }
+      dialect = request["dialect"].GetString();
+      // TODO: Check me
+      response.AddMember("ok", true, allocator);
+    } else if (cmd == "run") {
+      if (!started) {
+        throw runtime_error("Bowtie hasn't started!");
+      }
 
-            response.AddMember("seq", request["seq"], allocator);
-            Value results(kArrayType);
+      response.AddMember("seq", request["seq"], allocator);
+      Value results(kArrayType);
 
-            valijson::Validator validator;
+      valijson::Validator validator;
 
-            const Value& testCase = request["case"].GetObject();
+      const Value &testCase = request["case"].GetObject();
 
-            Schema schema;
-            SchemaParser parser(DIALECTS.at(dialect));
-            RapidJsonAdapter schemaAdapter(testCase["schema"]);
+      Schema schema;
+      SchemaParser parser(DIALECTS.at(dialect));
+      RapidJsonAdapter schemaAdapter(testCase["schema"]);
 
-            auto registry = Registry { &testCase["registry"] };
-            parser.populateSchema(
-                    schemaAdapter,
-                    schema,
-                    bind(&Registry::fetchDocument, registry, _1),
-                    freeDocument
-            );
+      auto registry = Registry{&testCase["registry"]};
+      parser.populateSchema(schemaAdapter, schema,
+                            bind(&Registry::fetchDocument, registry, _1),
+                            freeDocument);
 
-            for (auto& each : testCase["tests"].GetArray()) {
-                RapidJsonAdapter instance(each["instance"]);
+      for (auto &each : testCase["tests"].GetArray()) {
+        RapidJsonAdapter instance(each["instance"]);
 
-                Value result(kObjectType);
-                result.AddMember("valid", validator.validate(schema, instance, NULL), allocator);
+        Value result(kObjectType);
+        result.AddMember("valid", validator.validate(schema, instance, NULL),
+                         allocator);
 
-                results.PushBack(result, allocator);
-            }
+        results.PushBack(result, allocator);
+      }
 
-            response.AddMember("results", results, allocator);
-        } else if (cmd == "stop") {
-            if (!started) { throw runtime_error("Bowtie hasn't started!"); }
-            return 0;
-        } else {
-            string message = "Unknown command: ";
-            message += cmd;
-            throw runtime_error(message);
-        }
-
-        StringBuffer buffer;
-        Writer<StringBuffer> writer(buffer);
-        response.Accept(writer);
-
-        std::cout << buffer.GetString() << std::endl;
+      response.AddMember("results", results, allocator);
+    } else if (cmd == "stop") {
+      if (!started) {
+        throw runtime_error("Bowtie hasn't started!");
+      }
+      return 0;
+    } else {
+      string message = "Unknown command: ";
+      message += cmd;
+      throw runtime_error(message);
     }
+
+    StringBuffer buffer;
+    Writer<StringBuffer> writer(buffer);
+    response.Accept(writer);
+
+    std::cout << buffer.GetString() << std::endl;
+  }
 }
--- implementations/dotnet-json-everything/Program.cs
+++ implementations/dotnet-json-everything/Program.cs
@@ -5,148 +5,129 @@ using System.Text.Json.Nodes;
 
 using Json.Schema;
 
-
 bool started = false;
 var options = new ValidationOptions();
 
-
-var drafts = new Dictionary<string, Draft>
-{
-    {"https://json-schema.org/draft/2020-12/schema", Draft.Draft202012},
-    {"https://json-schema.org/draft/2019-09/schema", Draft.Draft201909},
-    {"http://json-schema.org/draft-07/schema#", Draft.Draft7},
-    {"http://json-schema.org/draft-06/schema#", Draft.Draft6},
+var drafts = new Dictionary<string, Draft> {
+  { "https://json-schema.org/draft/2020-12/schema", Draft.Draft202012 },
+  { "https://json-schema.org/draft/2019-09/schema", Draft.Draft201909 },
+  { "http://json-schema.org/draft-07/schema#", Draft.Draft7 },
+  { "http://json-schema.org/draft-06/schema#", Draft.Draft6 },
 };
 
+while (Console.ReadLine() is {} line && line != "") {
+  var root = JsonNode.Parse(line);
+  var cmd = root["cmd"].GetValue<string>();
+  switch (cmd) {
+  case "start":
+    var version = root["version"];
+    if (version.GetValue<int>() != 1) {
+      throw new UnknownVersion(version);
+    }
+    started = true;
+    var startResult = new JsonObject {
+      ["ready"] = true,
+      ["version"] = 1,
+      ["implementation"] =
+          new JsonObject {
+            ["language"] = "dotnet",
+            ["name"] = "json-everything",
+            ["homepage"] = "https://json-everything.net/json-schema/",
+            ["issues"] =
+                "https://github.com/gregsdennis/json-everything/issues",
 
-while (Console.ReadLine() is { } line && line != "")
-{
-	var root = JsonNode.Parse(line);
-	var cmd = root["cmd"].GetValue<string>();
-	switch (cmd)
-	{
-		case "start":
-			var version = root["version"];
-			if (version.GetValue<int>() != 1) {
-				throw new UnknownVersion(version);
-			}
-			started = true;
-			var startResult = new JsonObject
-			{
-				["ready"] = true,
-				["version"] = 1,
-				["implementation"] = new JsonObject
-				{
-					["language"] = "dotnet",
-					["name"] = "json-everything",
-					["homepage"] = "https://json-everything.net/json-schema/",
-					["issues"] = "https://github.com/gregsdennis/json-everything/issues",
+            ["dialects"] =
+                new JsonArray {
+                  "https://json-schema.org/draft/2020-12/schema",
+                  "https://json-schema.org/draft/2019-09/schema",
+                  "http://json-schema.org/draft-07/schema#",
+                  "http://json-schema.org/draft-06/schema#",
+                },
+          },
+    };
+    Console.WriteLine(startResult.ToJsonString());
+    break;
 
-					["dialects"] = new JsonArray
-                                        {
-                                            "https://json-schema.org/draft/2020-12/schema",
-                                            "https://json-schema.org/draft/2019-09/schema",
-                                            "http://json-schema.org/draft-07/schema#",
-                                            "http://json-schema.org/draft-06/schema#",
-                                        },
-				},
-			};
-			Console.WriteLine(startResult.ToJsonString());
-			break;
+  case "dialect":
+    if (!started) {
+      throw new NotStarted();
+    }
+    options = new ValidationOptions {
+      ValidateAs = drafts[root["dialect"].GetValue<string>()]
+    };
 
-                case "dialect":
-			if (!started) {
-				throw new NotStarted();
-			}
-                        options = new ValidationOptions{
-                                ValidateAs = drafts[root["dialect"].GetValue<string>()]
-                        };
+    var dialectResult = new JsonObject {
+      ["ok"] = true,
+    };
+    Console.WriteLine(dialectResult.ToJsonString());
+    break;
 
-			var dialectResult = new JsonObject
-			{
-                          ["ok"] = true,
-                        };
-			Console.WriteLine(dialectResult.ToJsonString());
-			break;
+  case "run":
+    if (!started) {
+      throw new NotStarted();
+    }
 
-		case "run":
-			if (!started) {
-				throw new NotStarted();
-			}
+    var testCase = root["case"];
+    var schemaText = testCase["schema"];
+    var registry = testCase["registry"];
 
-			var testCase = root["case"];
-			var schemaText = testCase["schema"];
-                        var registry = testCase["registry"];
+    options.SchemaRegistry.Fetch =
+        uri => { return registry[uri.ToString()].Deserialize<JsonSchema>(); };
 
-                        options.SchemaRegistry.Fetch = uri =>
-                        {
-                            return registry[uri.ToString()].Deserialize<JsonSchema>();
-                        };
+    var schema = schemaText.Deserialize<JsonSchema>();
+    var tests = testCase["tests"].AsArray();
 
-			var schema = schemaText.Deserialize<JsonSchema>();
-			var tests = testCase["tests"].AsArray();
+    try {
+      var results = new JsonArray();
 
-                        try {
-                            var results = new JsonArray();
+      foreach (var test in tests) {
+        var validationResult = schema.Validate(test["instance"], options);
+        var testResult = new JsonObject {
+          ["valid"] = validationResult.IsValid,
+        };
+        results.Add(testResult);
+      };
 
-                            foreach (var test in tests)
-                            {
-                                    var validationResult = schema.Validate(test["instance"], options);
-                                    var testResult = new JsonObject
-                                    {
-                                            ["valid"] = validationResult.IsValid,
-                                    };
-                                    results.Add(testResult);
-                            };
+      var runResult = new JsonObject {
+        ["seq"] = root["seq"].GetValue<int>(),
+        ["results"] = results,
+      };
+      Console.WriteLine(runResult.ToJsonString());
+    } catch (Exception e) {
+      var errorResult = new JsonObject {
+        ["seq"] = root["seq"].GetValue<int>(),
+        ["errored"] = true,
+        ["context"] =
+            new JsonObject {
+              ["Exception"] = e.ToString(),
+              ["StackTrace"] = Environment.StackTrace,
+            },
+      };
+      Console.WriteLine(errorResult.ToJsonString());
+    }
+    break;
 
-                            var runResult = new JsonObject
-                            {
-                                    ["seq"] = root["seq"].GetValue<int>(),
-                                    ["results"] = results,
-                            };
-                            Console.WriteLine(runResult.ToJsonString());
-                        }
-                        catch (Exception e)
-                        {
-                            var errorResult = new JsonObject
-                            {
-                                    ["seq"] = root["seq"].GetValue<int>(),
-                                    ["errored"] = true,
-                                    ["context"] = new JsonObject {
-                                        ["Exception"] = e.ToString(),
-                                        ["StackTrace"] = Environment.StackTrace,
-                                    },
-                            };
-                            Console.WriteLine(errorResult.ToJsonString());
-                        }
-			break;
+  case "stop":
+    if (!started) {
+      throw new NotStarted();
+    }
+    Environment.Exit(0);
+    break;
 
-		case "stop":
-			if (!started) {
-				throw new NotStarted();
-			}
-			Environment.Exit(0);
-			break;
+  case null:
+    throw new UnknownCommand("Missing command!");
 
-		case null:
-			throw new UnknownCommand("Missing command!");
-
-		default:
-			throw new UnknownCommand(cmd);
-
-	}
+  default:
+    throw new UnknownCommand(cmd);
+  }
 }
 
-class UnknownCommand : Exception
-{
-    public UnknownCommand(string message) {}
+class UnknownCommand : Exception {
+  public UnknownCommand(string message) {}
 }
 
-class UnknownVersion : Exception
-{
-    public UnknownVersion(JsonNode version) {}
+class UnknownVersion : Exception {
+  public UnknownVersion(JsonNode version) {}
 }
 
-class NotStarted : Exception
-{
-}
+class NotStarted : Exception {}
