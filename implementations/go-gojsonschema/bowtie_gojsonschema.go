fd8f4ce5c2717fed0869e19420a9be20a3cdd370
Julian Berman <Julian@GrayVines.com>
2023-03-10 11:53:41 -0500

    Add support for xeipuuv/gojsonschema
    This implementation is used by Helm (to validate its charts).

---
+++ implementations/go-gojsonschema/bowtie_gojsonschema.go
@@ -0,0 +1,179 @@
+package main
+
+import (
+	"bufio"
+	"encoding/json"
+	"fmt"
+	"log"
+	"os"
+	"runtime/debug"
+	"strings"
+
+	"github.com/xeipuuv/gojsonschema"
+)
+
+var drafts = map[string]gojsonschema.Draft{
+	"http://json-schema.org/draft-07/schema#": gojsonschema.Draft7,
+	"http://json-schema.org/draft-06/schema#": gojsonschema.Draft6,
+	"http://json-schema.org/draft-04/schema#": gojsonschema.Draft4,
+}
+
+func main() {
+	var started = false
+	var draft gojsonschema.Draft = gojsonschema.Hybrid
+
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		var request map[string]interface{}
+
+		decoder := json.NewDecoder(strings.NewReader(scanner.Text()))
+		decoder.UseNumber()
+		err := decoder.Decode(&request)
+		if err != nil {
+			fmt.Printf("could not unmarshal json: %s\n", err)
+			return
+		}
+
+		switch request["cmd"] {
+		case "start":
+			started = true
+			rawVersion, ok := request["version"]
+			if !ok {
+				panic("No version!")
+			}
+			version := rawVersion.(json.Number)
+			if v, err := version.Float64(); err != nil || v != 1 {
+				panic("Not version 1!")
+			}
+			data := map[string]interface{}{
+				"ready":   true,
+				"version": 1,
+				"implementation": map[string]interface{}{
+					"language": "go",
+					"name":     "gojsonschema",
+					"version":  gojsonschemaVersion(),
+					"homepage": "https://github.com/xeipuuv/gojsonschema",
+					"issues":   "https://github.com/xeipuuv/gojsonschema/issues",
+					"dialects": []string{
+						"http://json-schema.org/draft-07/schema#",
+						"http://json-schema.org/draft-06/schema#",
+						"http://json-schema.org/draft-04/schema#",
+					},
+				},
+			}
+			printJSON(data)
+		case "dialect":
+			if !started {
+				panic("Not started!")
+			}
+			rawDialect, ok := request["dialect"]
+			if !ok {
+				panic("No dialect!")
+			}
+			dialect := rawDialect.(string)
+			draft = drafts[dialect]
+			data := map[string]interface{}{"ok": draft != gojsonschema.Hybrid}
+			printJSON(data)
+		case "run":
+			if !started {
+				panic("Not started!")
+			}
+			testCase, ok := request["case"].(map[string]interface{})
+			if !ok {
+				panic("No case!")
+			}
+
+			loader := gojsonschema.NewSchemaLoader()
+			if draft != gojsonschema.Hybrid {
+				loader.Draft = draft
+				loader.AutoDetect = false
+			}
+			registry, ok := testCase["registry"].(map[string]interface{})
+			if ok {
+				for uri, remoteSchema := range registry {
+					remoteLoader := gojsonschema.NewGoLoader(remoteSchema.(map[string]interface{}))
+					loader.AddSchema(uri, remoteLoader)
+				}
+			}
+
+			schemaLoader := gojsonschema.NewGoLoader(testCase["schema"])
+			schema, err := loader.Compile(schemaLoader)
+			if err != nil {
+				printError(request["seq"].(json.Number), err)
+				break
+			}
+
+			var results []map[string]interface{}
+			for _, test := range testCase["tests"].([]interface{}) {
+				var result map[string]interface{}
+
+				instanceLoader := gojsonschema.NewGoLoader(test.(map[string]interface{})["instance"])
+				validationResult, err := schema.Validate(instanceLoader)
+				if err == nil {
+					result = map[string]interface{}{
+						"valid": validationResult.Valid(),
+					}
+				} else {
+					result = map[string]interface{}{
+						"errored": true,
+						"context": map[string]interface{}{
+							"message": err.Error(),
+						},
+					}
+				}
+				results = append(results, result)
+			}
+
+			data := map[string]interface{}{
+				"seq":     request["seq"],
+				"results": results,
+			}
+			printJSON(data)
+		case "stop":
+			if !started {
+				panic("Not started!")
+			}
+			os.Exit(0)
+		default:
+			panic("Unknown command")
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Println(err)
+	}
+}
+
+func gojsonschemaVersion() string {
+	buildInfo, ok := debug.ReadBuildInfo()
+	if !ok {
+		panic("Failed to read build info")
+	}
+
+	var version = ""
+	for _, dep := range buildInfo.Deps {
+		if strings.Contains(dep.Path, "github.com/xeipuuv/gojsonschema") {
+			version = dep.Version
+			break
+		}
+	}
+	return version
+}
+
+func printJSON(v interface{}) {
+	encoder := json.NewEncoder(os.Stdout)
+	if err := encoder.Encode(v); err != nil {
+		panic("Failed sending a response!")
+	}
+}
+
+func printError(seq json.Number, err error) {
+	data := map[string]interface{}{
+		"seq":     seq,
+		"errored": true,
+		"context": map[string]interface{}{
+			"message": err.Error(),
+		},
+	}
+	printJSON(data)
+}
---
+++ implementations/go-gojsonschema/go.mod
@@ -0,0 +1,9 @@
+module bowtie-json-schema/go-gojsonschema
+
+go 1.20
+
+require (
+	github.com/xeipuuv/gojsonpointer v0.0.0-20180127040702-4e3ac2762d5f // indirect
+	github.com/xeipuuv/gojsonreference v0.0.0-20180127040603-bd5ef7bd5415 // indirect
+	github.com/xeipuuv/gojsonschema v1.2.0 // indirect
+)
