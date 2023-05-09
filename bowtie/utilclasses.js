e0aaf623dba9df6cdeed23b86b501bf6423997b5
Agnivesh Chaubey <agniveshvapi@gmail.com>
2023-05-09 17:24:57 +0530

    Summary class created in JS

--- bowtie/utilclasses.js
+++ bowtie/utilclasses.js
@@ -50,3 +50,168 @@ class _Summary {
     this.implementations = implementations;
   }
 }
+
+//////////////////////////////////
+// Sunnary class
+
+
+class Summary {
+  implementations = [];
+  _combined = {};
+  did_fail_fast = false;
+  counts = {};
+
+  constructor() {
+    this.implementations = [];
+    this._combined = {};
+    this.did_fail_fast = false;
+    this.counts = {};
+  }
+
+  __attrs_post_init__() {
+    this.counts = {};
+    for (let i = 0; i < this.implementations.length; i++) {
+      this.counts[this.implementations[i]["image"]] = new Count();
+    }
+  }
+
+  get total_cases() {
+    const counts = Object.values(this.counts).map((count) => count.total_cases);
+    if (new Set(counts).size !== 1) {
+      const summary = Object.entries(this.counts)
+        .map(([key, count]) => `  ${key.split("/").pop()}: ${count.total_cases}`)
+        .join("\n");
+      throw new InvalidBowtieReport(`Inconsistent number of cases run:\n\n${summary}`);
+    }
+    return counts[0];
+  }
+
+  get errored_cases() {
+    return Object.values(this.counts).reduce((sum, count) => sum + count.errored_cases, 0);
+  }
+
+  get total_tests() {
+    const counts = Object.values(this.counts).map((count) => count.total_tests);
+    if (new Set(counts).size !== 1) {
+      throw new InvalidBowtieReport(`Inconsistent number of tests run: ${this.counts}`);
+    }
+    return counts[0];
+  }
+
+  get failed_tests() {
+    return Object.values(this.counts).reduce((sum, count) => sum + count.failed_tests, 0);
+  }
+
+  get errored_tests() {
+    return Object.values(this.counts).reduce((sum, count) => sum + count.errored_tests, 0);
+  }
+
+  get skipped_tests() {
+    return Object.values(this.counts).reduce((sum, count) => sum + count.skipped_tests, 0);
+  }
+
+  add_case_metadata(seq, caseMetadata) {
+    const results = caseMetadata.tests.map((test) => [test, {}]);
+    this._combined[seq] = { case: caseMetadata, results: results };
+  }
+
+  see_error(implementation, seq, context, caught) {
+    const count = this.counts[implementation];
+    count.total_cases += 1;
+    count.errored_cases += 1;
+
+    const caseMetadata = this._combined[seq]["case"];
+    count.total_tests += caseMetadata.tests.length;
+    count.errored_tests += caseMetadata.tests.length;
+  }
+
+  see_result(result) {
+    const count = this.counts[result.implementation];
+    count.total_cases += 1;
+
+    const combined = this._combined[result.seq]["results"];
+
+    for (let i = 0; i < result.compare().length; i++) {
+      const [test, failed] = result.compare()[i];
+      const [, seen] = combined[i];
+      count.total_tests += 1;
+      if (test.skipped) {
+        count.skipped_tests += 1;
+        seen[result.implementation] = [test.reason, "skipped"];
+      } else if (test.errored) {
+        count.errored_tests += 1;
+        seen[result.implementation] = [test.reason, "errored"];
+      } else {
+        if (failed) {
+          count.failed_tests += 1;
+        }
+        seen[result.implementation] = [test, failed];
+      }
+    }
+  }
+
+  see_skip(skipped) {
+    const count = this.counts[skipped.implementation];
+    count.total_cases += 1;
+  
+    const case_data = this._combined[skipped.seq]['case'];
+    count.total_tests += case_data.tests.length;
+    count.skipped_tests += case_data.tests.length;
+  
+    for (const [_, seen] of this._combined[skipped.seq]['results']) {
+      const message = skipped.issue_url || skipped.message || 'skipped';
+      seen[skipped.implementation] = [message, 'skipped'];
+    }
+  }
+  
+  see_maybe_fail_fast(did_fail_fast) {
+    this.didFailFast = did_fail_fast;
+  }
+  
+  case_results() {
+    const result = [];
+    for (const each of Object.values(this._combined)) {
+      result.push([each['case'], each['registry'] || {}, each['results']]);
+    }
+    return result;
+  }
+  
+  *flat_results() {
+    for (const [seq, each] of Object.entries(this._combined).sort(([seq1], [seq2]) => seq1 - seq2)) {
+      const case_data = each['case'];
+      yield [
+        seq,
+        case_data.description,
+        case_data.schema,
+        each['registry'] || {},
+        each['results'],
+      ];
+    }
+  }
+  
+  generate_badges(target_dir, dialect) {
+    const label = _DIALECT_URI_TO_SHORTNAME[dialect];
+    for (const impl of this.implementations) {
+      if (!impl['dialects'].includes(dialect)) continue;
+  
+      const name = impl['name'];
+      const lang = impl['language'];
+      const counts = this.counts[impl['image']];
+      const total = counts.total_tests;
+      const passed = total - counts.failed_tests - counts.errored_tests - counts.skipped_tests;
+      const pct = (passed / total) * 100;
+      const impl_dir = target_dir.join(`${lang}-${name}`);
+      fs.mkdirSync(impl_dir, { recursive: true });
+  
+      const [r, g, b] = [100 - Math.floor(pct), Math.floor(pct), 0];
+      const badge = {
+        schemaVersion: 1,
+        label,
+        message: `${Math.floor(pct)}% Passing`,
+        color: `${r.toString(16).padStart(2, '0')}${g.toString(16).padStart(2, '0')}${b.toString(16).padStart(2, '0')}`,
+      };
+      const badgePath = implDir.join(`${label.replace(' ', '_')}.json`);
+      fs.writeFileSync(badgePath, JSON.stringify(badge));
+    }
+  }
+}
