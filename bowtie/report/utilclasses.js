eb31e2716c26652af298ade6af8a5d14204eb3ca
Agnivesh Chaubey <agniveshvapi@gmail.com>
2023-06-02 09:28:00 +0530

    utilities function added

--- bowtie/report/utilclasses.js
+++ bowtie/report/utilclasses.js
@@ -1,77 +1,79 @@
-// import attrs from 'https://cdn.jsdelivr.net/npm/attrs@2.2.0/dist/attrs.min.js';
+export function schemaDisplay(parentBodyId, code, targetId, rowClass) {
+  let currentParentBodyId = null;
+  const displayBody = document.getElementById("display-body");
 
-// import * as importlib from 'https://unpkg.com/importlib';
+  if (parentBodyId !== currentParentBodyId) {
+    currentParentBodyId = parentBodyId;
+    displayBody.classList.remove("d-none");
+    displayBody.classList.add("d-block");
+    document.getElementById("instance-info").innerHTML = "";
 
-class InvalidBowtieReport extends Error {
-  constructor(message) {
-    super(message);
-    this.name = "InvalidBowtieReport";
+    const isDisplayed =
+      displayBody.parentElement === document.getElementById(parentBodyId);
+
+    if (isDisplayed) {
+      displayBody.parentElement.removeChild(displayBody);
+    } else {
+      // Add Highlight Row Functionality to clicked accordion only
+      rowHighlightListener(rowClass);
+      displayCode(code, targetId);
+      const accordionBody = document.querySelector(`#${parentBodyId}`);
+      accordionBody.insertBefore(displayBody, accordionBody.firstChild);
+    }
   }
 }
 
-const _DIALECT_URI_TO_SHORTNAME = {
-  "https://json-schema.org/draft/2020-12/schema": "Draft 2020-12",
-  "https://json-schema.org/draft/2019-09/schema": "Draft 2019-09",
-  "http://json-schema.org/draft-07/schema#": "Draft 7",
-  "http://json-schema.org/draft-06/schema#": "Draft 6",
-  "http://json-schema.org/draft-04/schema#": "Draft 4",
-  "http://json-schema.org/draft-03/schema#": "Draft 3",
-};
-
-function writer(file = process.stdout) {
-  return (result) => {
-    file.write(`${JSON.stringify(result)}\n`);
-  };
-}
-
-export class RunInfo {
-  constructor(started, bowtie_version, dialect, _implementations) {
-    this.started = started;
-    this.bowtie_version = bowtie_version;
-    this.dialect = dialect;
-    this._implementations = _implementations;
-  }
-
-  get dialect_shortname() {
-    return _DIALECT_URI_TO_SHORTNAME[this.dialect] || this.dialect;
-  }
-
-  static from_implementations(implementations, dialect) {
-    const now = new Date();
-    return new RunInfo(
-      now.toISOString(),
-      importlib.metadata.version("bowtie-json-schema"),
-      dialect,
-      Object.fromEntries(
-        implementations.map((implementation) => [
-          implementation.name,
-          {
-            ...implementation.metadata,
-            image: implementation.name,
-          },
-        ]),
-      ),
-    );
-  }
-
-  create_summary() {
-    // console.log(Object.values(this._implementations))
-    // console.log((this._implementations))
-    return new _Summary({
-      implementations: Object.values(this._implementations),
+function rowHighlightListener(rowClass) {
+  const instanceRows = document.getElementsByClassName(rowClass);
+  for (let i = 0; i < instanceRows.length; i++) {
+    const currentInstanceRow = instanceRows[i];
+    currentInstanceRow.addEventListener("click", function () {
+      const activeElements = document.getElementsByClassName("table-active");
+      for (let j = 0; j < activeElements.length; j++) {
+        activeElements[j].classList.remove("table-active");
+      }
+      currentInstanceRow.classList.add("table-active");
     });
   }
 }
 
-RunInfo._implementations = {
-  alias: "implementations",
-  validate: (value) => {
-    if (!Array.isArray(value)) {
-      throw new Error('The "implementations" attribute must be an array.');
-    }
-    return true;
-  },
-};
+function displayCode(code, targetId) {
+  const targetDisplay = document.getElementById(targetId);
+  displayBody.classList.remove("d-none");
+  displayBody.classList.add("d-block");
+  targetDisplay.innerHTML = `<pre>${code}</pre>`;
+}
+
+export function to_icon(valid) {
+  if (valid === true) {
+    // Circular Checkmark SVG Icon
+    return `
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check-circle-fill" viewBox="0 0 16 16">
+          <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z" />
+        </svg>
+      `;
+  } else if (valid === false) {
+    // Circular cross (x) SVG Icon
+    return `
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-circle-fill" viewBox="0 0 16 16">
+          <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM5.354 4.646a.5.5 0 1 0-.708.708L7.293 8l-2.647 2.646a.5.5 0 0 0 .708.708L8 8.707l2.646 2.647a.5.5 0 0 0 .708-.708L8.707 8l2.647-2.646a.5.5 0 0 0-.708-.708L8 7.293 5.354 4.646z" />
+        </svg>
+      `;
+  } else {
+    // Circular warning (!) SVG Icon
+    return `
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-exclamation-octagon" viewBox="0 0 16 16">
+          <path d="M4.54.146A.5.5 0 0 1 4.893 0h6.214a.5.5 0 0 1 .353.146l4.394 4.394a.5.5 0 0 1 .146.353v6.214a.5.5 0 0 1-.146.353l-4.394 4.394a.5.5 0 0 1-.353.146H4.893a.5.5 0 0 1-.353-.146L.146 11.46A.5.5 0 0 1 0 11.107V4.893a.5.5 0 0 1 .146-.353L4.54.146zM5.1 1 1 5.1v5.8L5.1 15h5.8l4.1-4.1V5.1L10.9 1H5.1z" />
+          <path d="M7.002 11a1 1 0 1 1 2 0 1 1 0 0 1-2 0zM7.1 4.995a.905.905 0 1 1 1.8 0l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 4.995z" />
+        </svg>
+      `;
+  }
+}
+
+
+
+
+
 
 //////////////////////////////////
 // Summary class
@@ -107,17 +109,17 @@ class _Summary {
 
   get total_cases() {
     const counts = new Set(
-      Object.values(this.counts).map((count) => count.total_cases),
+      Object.values(this.counts).map((count) => count.total_cases)
     );
     // console.log(counts)
     if (counts.size !== 1) {
       const summary = Object.entries(this.counts)
         .map(
-          ([each, count]) => `  ${each.split("/").pop()}: ${count.total_cases}`,
+          ([each, count]) => `  ${each.split("/").pop()}: ${count.total_cases}`
         )
         .join("\n");
       throw new InvalidBowtieReport(
-        `Inconsistent number of cases run:\n\n${summary}`,
+        `Inconsistent number of cases run:\n\n${summary}`
       );
     }
     return counts.values().next().value;
@@ -126,13 +128,13 @@ class _Summary {
   get errored_cases() {
     return Object.values(this.counts).reduce(
       (sum, count) => sum + count.errored_cases,
-      0,
+      0
     );
   }
 
   get total_tests() {
     const counts = new Set(
-      Object.values(this.counts).map((count) => count.total_tests),
+      Object.values(this.counts).map((count) => count.total_tests)
     );
     // console.log(counts)
     // if (counts.size !== 1) {
@@ -144,21 +146,21 @@ class _Summary {
   get failed_tests() {
     return Object.values(this.counts).reduce(
       (sum, count) => sum + count.failed_tests,
-      0,
+      0
     );
   }
 
   get errored_tests() {
     return Object.values(this.counts).reduce(
       (sum, count) => sum + count.errored_tests,
-      0,
+      0
     );
   }
 
   get skipped_tests() {
     return Object.values(this.counts).reduce(
       (sum, count) => sum + count.skipped_tests,
-      0,
+      0
     );
   }
 
@@ -243,7 +245,7 @@ class _Summary {
         } else {
           return 0;
         }
-      },
+      }
     );
     let sorted_CombinedObject = Object.fromEntries(sorted_CombinedArray);
     // console.log(sorted_CombinedObject)
