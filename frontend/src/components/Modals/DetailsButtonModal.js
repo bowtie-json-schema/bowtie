8faf59cb75ab1a20631ff40022f7fb15ab41c7a8
Agnivesh Chaubey <agniveshvapi@gmail.com>
2023-06-07 17:44:55 +0530

    unexpectedly valid case's details rendered in the modal

--- frontend/src/components/Modals/DetailsButtonModal.js
+++ frontend/src/components/Modals/DetailsButtonModal.js
@@ -37,6 +37,52 @@ export const DetailsButtonModal = ({ lines, summary }) => {
           // console.log(testResult)
           dataArray.push([testResult, testCase, testImplementation]);
         }
+        if (
+          testImplementation.results.every(
+            (element) =>
+              typeof element === "object" &&
+              Object.keys(element).length === 1 &&
+              "valid" in element
+          )
+        ) {
+          for (
+            let index = 0;
+            index < testImplementation.results.length;
+            index++
+          ) {
+            if (
+              testImplementation.results[index].valid !==
+              testImplementation.expected[index]
+            ) {
+              let validity = [];
+              if (testImplementation.expected[index] === false) {
+                validity.push(index);
+                var testResult = "unexpectedlyValid";
+                var testCase = caseArray.find(
+                  (obj) => obj.seq === testImplementation.seq
+                );
+                dataArray.push([
+                  testResult,
+                  testCase,
+                  testImplementation,
+                  validity,
+                ]);
+              } else {
+                validity.push[index];
+                var testResult = "unexpectedlyInvalid";
+                var testCase = caseArray.find(
+                  (obj) => obj.seq === testImplementation.seq
+                );
+                dataArray.push([
+                  testResult,
+                  testCase,
+                  testImplementation,
+                  validity,
+                ]);
+              }
+            }
+          }
+        }
       }
     });
     return dataArray;
@@ -77,7 +123,7 @@ export const DetailsButtonModal = ({ lines, summary }) => {
                 <div className="modal-body">
                   <div className="row row-cols-1 row-cols-md-2 g-4">
                     {result.map((eachResult, i) => {
-                      var [testResult, testCase, testImplementation] =
+                      var [testResult, testCase, testImplementation, validity] =
                         eachResult;
                       if (testResult === "skipped") {
                         return testCase.case.tests.map((test, index) => {
@@ -147,6 +193,31 @@ export const DetailsButtonModal = ({ lines, summary }) => {
                               </div>
                             </div>
                           );
+                        });
+                      } else if (testResult === "unexpectedlyValid") {
+                        return testCase.case.tests.map((test, index) => {
+                          if (validity.includes(index)) {
+                            return (
+                              <div
+                                className="col"
+                                key={`${testCase.seq}${index}`}
+                              >
+                                <div className="card border-warning mb-3">
+                                  <div className="card-body">
+                                    <h5 className="card-title">
+                                      {testCase.case.description}
+                                    </h5>
+                                    <p className="card-text">
+                                      {test.description}
+                                    </p>
+                                  </div>
+                                  <div className="card-footer text-muted text-center">
+                                    Unexpectedly valid
+                                  </div>
+                                </div>
+                              </div>
+                            );
+                          }
                         });
                       }
                     })}
