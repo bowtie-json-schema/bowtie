9e5072a002b8528c600f099f031b2755b4d9f1d2
Agnivesh Chaubey <agniveshvapi@gmail.com>
2023-06-15 22:09:30 +0530

    component design completed

--- frontend/src/components/DragAndDrop.js
+++ frontend/src/components/DragAndDrop.js
@@ -1,18 +1,87 @@
-import React from "react";
+import { useState, useRef } from "react";
 import "./DragAndDrop.css";
 import { CloudArrowUpFill } from "react-bootstrap-icons";
 
-const DragAndDrop = () => {
+function DragAndDrop() {
+  const [dragActive, setDragActive] = useState(false);
+  const inputRef = useRef(null);
+
+  const handleDrag = function (e) {
+    e.preventDefault();
+    e.stopPropagation();
+    if (e.type === "dragenter" || e.type === "dragover") {
+      setDragActive(true);
+    } else if (e.type === "dragleave") {
+      setDragActive(false);
+    }
+  };
+
+  const handleDrop = function (e) {
+    e.preventDefault();
+    e.stopPropagation();
+    setDragActive(false);
+    if (e.dataTransfer.files && e.dataTransfer.files[0]) {
+      handleFiles(e.dataTransfer.files);
+      console.log(e.dataTransfer.files[0]);
+    }
+  };
+
+  const handleChange = function (e) {
+    e.preventDefault();
+    if (e.target.files && e.target.files[0]) {
+      handleFiles(e.target.files);
+      console.log(e.target.files[0]);
+    }
+  };
+
+  const onButtonClick = () => {
+    inputRef.current.click();
+  };
+
+  const handleFiles = (file) => {};
+
   return (
-    <div className="center-content">
-      <div className="card" data-bs-theme="dark" id="dragAndDrop">
-        <div className="card-body d-flex flex-column align-items-center justify-content-center">
-          <CloudArrowUpFill size={80} />
-          <h5 className="card-text">Drag your local report here!</h5>
-        </div>
-      </div>
+    <div className="card-body d-grid justify-content-center mt-5">
+      <form
+        id="form-file-upload"
+        onDragEnter={handleDrag}
+        onSubmit={(e) => e.preventDefault()}
+      >
+        <input
+          ref={inputRef}
+          type="file"
+          id="input-file-upload"
+          className="d-none"
+          accept=".json,.jsonl"
+          multiple={false}
+          onChange={handleChange}
+        />
+
+        <label
+          id="label-file-upload"
+          htmlFor="input-file-upload"
+          className={dragActive ? "drag-active" : ""}
+        >
+          <div className="text-center">
+            <CloudArrowUpFill size={80} />
+            <p className="card-text">Drag your local report here!</p>
+            <button className="btn btn-primary" onClick={onButtonClick}>
+              Upload report
+            </button>
+          </div>
+        </label>
+        {dragActive && (
+          <div
+            id="drag-file-element"
+            onDragEnter={handleDrag}
+            onDragLeave={handleDrag}
+            onDragOver={handleDrag}
+            onDrop={handleDrop}
+          ></div>
+        )}
+      </form>
     </div>
   );
-};
+}
 
 export default DragAndDrop;
