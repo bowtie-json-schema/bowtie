8dc15bb672aa2d834872074ec8164df182d9e71a
Harrel <marcin.slowik95@gmail.com>
2023-06-26 18:56:50 +0200

    review: map dialect name

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,61 +1,70 @@
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 
-import { createRoot } from "react-dom/client";
-import ReportDataHandler from "./ReportDataHandler";
-import { createHashRouter, RouterProvider } from "react-router-dom";
-import ThemeContextProvider from "./context/ThemeContext";
-import { MainContainer } from "./MainContainer";
-import { BowtieVersionContextProvider } from "./context/BowtieVersionContext";
-import { DragAndDrop } from "./components/DragAndDrop/DragAndDrop";
+import {createRoot} from 'react-dom/client'
+import ReportDataHandler from './ReportDataHandler'
+import {createHashRouter, RouterProvider} from 'react-router-dom'
+import ThemeContextProvider from './context/ThemeContext'
+import {MainContainer} from './MainContainer'
+import {BowtieVersionContextProvider} from './context/BowtieVersionContext'
+import {DragAndDrop} from './components/DragAndDrop/DragAndDrop'
 
 const reportUrl =
-  import.meta.env.MODE === "development"
-    ? "https://bowtie-json-schema.github.io/bowtie"
-    : import.meta.env.BASE_URL;
-const titleTag = document.getElementsByTagName("title")[0];
+  import.meta.env.MODE === 'development'
+    ? 'https://bowtie-json-schema.github.io/bowtie'
+    : import.meta.env.BASE_URL
+const titleTag = document.getElementsByTagName('title')[0]
+const dialectToName = {
+  'draft2020-12': 'Draft 2020-12',
+  'draft2019-09': 'Draft 2019-09',
+  'draft7': 'Draft 7',
+  'draft6': 'Draft 6',
+  'draft4': 'Draft 4',
+  'draft3': 'Draft 3'
+}
 
 const fetchReportData = async (dialect) => {
-  titleTag.textContent = `Bowtie - ${dialect}`;
-  const response = await fetch(`${reportUrl}/${dialect}.json`);
-  const jsonl = await response.text();
+  const dialectName = dialectToName[dialect] ?? dialect
+  titleTag.textContent = `Bowtie - ${dialectName}`
+  const response = await fetch(`${reportUrl}/${dialect}.json`)
+  const jsonl = await response.text()
   return jsonl
     .trim()
     .split(/\r?\n/)
-    .map((line) => JSON.parse(line));
-};
+    .map((line) => JSON.parse(line))
+}
 
 const router = createHashRouter([
   {
-    path: "/",
+    path: '/',
     Component: MainContainer,
 
     children: [
       {
         index: true,
         Component: ReportDataHandler,
-        loader: async () => fetchReportData("draft2020-12"),
+        loader: async () => fetchReportData('draft2020-12')
       },
       {
-        path: "/:draftName",
+        path: '/:draftName',
         Component: ReportDataHandler,
-        loader: async ({ params }) => fetchReportData(params.draftName),
+        loader: async ({params}) => fetchReportData(params.draftName)
       },
       {
-        path: "/local-report",
-        Component: DragAndDrop,
-      },
+        path: '/local-report',
+        Component: DragAndDrop
+      }
     ]
   }
-]);
+])
 
-document.addEventListener("DOMContentLoaded", () => {
-  const root = createRoot(document.getElementById("root"));
+document.addEventListener('DOMContentLoaded', () => {
+  const root = createRoot(document.getElementById('root'))
   root.render(
     <ThemeContextProvider>
       <BowtieVersionContextProvider>
-        <RouterProvider router={router} />
+        <RouterProvider router={router}/>
       </BowtieVersionContextProvider>
-    </ThemeContextProvider>,
-  );
-});
+    </ThemeContextProvider>
+  )
+})
