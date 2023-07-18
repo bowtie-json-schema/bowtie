558eb7a740abbdb7c8337f15f25e0bca8af1877a
Agnivesh Chaubey <agniveshvapi@gmail.com>
2023-07-19 00:17:16 +0530

    add start and stop function

--- implementations/php-json-schema/bowtieJsonSchema.php
+++ implementations/php-json-schema/bowtieJsonSchema.php
@@ -1,5 +1,38 @@
 <?php
 
-echo "Hello, world! It is currently " . date('Y-m-d H:i:s') . " PST.";
+$STARTED = false;
 
-?>
+$cmds = [
+    'start' => function($request) use (&$STARTED) {
+        assert($request['version'] == 1, 'Wrong version!');
+        $STARTED = true;
+        return [
+            'ready' => true,
+            'version' => 1,
+            'implementation' => [
+                'language' => 'php',
+                'name' => 'json-schema',
+                'homepage' => 'https://opis.io/json-schema',
+                'issues' => 'https://github.com/opis/json-schema/issues',
+                'dialects' => [
+                    'http://json-schema.org/draft/2020-12/schema#',
+                    'http://json-schema.org/draft-2019-09/schema#',
+                    'http://json-schema.org/draft-07/schema#',
+                    'http://json-schema.org/draft-06/schema#',
+                    ,
+                ],
+            ],
+        ];
+    },
+
+    'stop' => function() use (&$STARTED) {
+        assert($STARTED, 'Not started!');
+        exit(0);
+    },
+];
+
+while ($line = fgets(STDIN)) {
+    $request = json_decode($line, true);
+    $response = $cmds[$request['cmd']]($request);
+    fwrite(STDOUT, json_encode($response) . "\n");
+}
