3ad5661b33db07f8a3fc8f7401eddae738e8d8fc
Harrel <marcin.slowik95@gmail.com>
2023-08-08 23:24:23 +0200

    restore "cases" id for anchor

--- frontend/src/components/Cases/CasesSection.jsx
+++ frontend/src/components/Cases/CasesSection.jsx
@@ -4,7 +4,7 @@ import { Accordion } from "react-bootstrap";
 const CasesSection = ({ reportData }) => {
   const implementations = Array.from(reportData.implementations.values());
   return (
-    <Accordion>
+    <Accordion id='cases'>
       {Array.from(reportData.cases.entries()).map(([seq, caseData], index) => (
         <CaseItem
           key={index}
