7ebf8155e523b2a977d44607449a6eeab7afcdd7
Harrel <marcin.slowik95@gmail.com>
2023-08-08 22:30:57 +0200

    feat: lazy load accordion content

--- frontend/src/components/Cases/AccordionItem.jsx
+++ frontend/src/components/Cases/AccordionItem.jsx
@@ -1,9 +1,60 @@
 import AccordionSvg from "./AccordionSvg";
 import SchemaDisplay from "./SchemaDisplay";
-import { useState } from "react";
+import {useEffect, useState, useTransition} from 'react'
+import LoadingAnimation from '../LoadingAnimation'
+
+const Content = ({ seq, caseData, implementations }) => {
+  const [instance, setInstance] = useState();
+
+  return (
+  <div id={`accordion-body${seq}`} className="accordion-body">
+    <SchemaDisplay schema={caseData.schema} instance={instance} />
+    <table className="table table-hover">
+      <thead>
+      <tr>
+        <td scope="col">
+          <b>Tests</b>
+        </td>
+        {implementations.map((implementation) => (
+          <td
+            className="text-center"
+            scope="col"
+            key={
+              implementation.metadata.name +
+              implementation.metadata.language
+            }
+          >
+            <b>{implementation.metadata.name + " "}</b>
+            <small className="text-muted">
+              {implementation.metadata.language}
+            </small>
+          </td>
+        ))}
+      </tr>
+      </thead>
+      <tbody>
+      {caseData.tests.map((test, index) => (
+        <tr key={index} onClick={() => setInstance(test.instance)}>
+          <td>
+            <p className="m-0">{test.description}</p>
+          </td>
+          {implementations.map((impl, i) => (
+            <AccordionSvg key={i} result={impl.cases.get(seq)[index]} />
+          ))}
+        </tr>
+      ))}
+      </tbody>
+    </table>
+  </div>
+)
+}
 
 const AccordionItem = ({ seq, caseData, implementations }) => {
-  const [instance, setInstance] = useState();
+  const [content, setContent] = useState(<LoadingAnimation/>);
+  const [, startTransition] = useTransition();
+  useEffect(() => {
+    startTransition(() => setContent(<Content seq={seq} caseData={caseData} implementations={implementations}/> ))
+  }, [seq, caseData, implementations])
   return (
     <div className="accordion-item">
       <h2 className="accordion-header" id={`case-${seq}-heading`}>
@@ -24,45 +75,7 @@ const AccordionItem = ({ seq, caseData, implementations }) => {
         aria-labelledby={`case-${seq}-heading`}
         data-bs-parent="#cases"
       >
-        <div id={`accordion-body${seq}`} className="accordion-body">
-          <SchemaDisplay schema={caseData.schema} instance={instance} />
-          <table className="table table-hover">
-            <thead>
-              <tr>
-                <td scope="col">
-                  <b>Tests</b>
-                </td>
-                {implementations.map((implementation) => (
-                  <td
-                    className="text-center"
-                    scope="col"
-                    key={
-                      implementation.metadata.name +
-                      implementation.metadata.language
-                    }
-                  >
-                    <b>{implementation.metadata.name + " "}</b>
-                    <small className="text-muted">
-                      {implementation.metadata.language}
-                    </small>
-                  </td>
-                ))}
-              </tr>
-            </thead>
-            <tbody>
-              {caseData.tests.map((test, index) => (
-                <tr key={index} onClick={() => setInstance(test.instance)}>
-                  <td>
-                    <p className="m-0">{test.description}</p>
-                  </td>
-                  {implementations.map((impl, i) => (
-                    <AccordionSvg key={i} result={impl.cases.get(seq)[index]} />
-                  ))}
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        </div>
+        {content}
       </div>
     </div>
   );
