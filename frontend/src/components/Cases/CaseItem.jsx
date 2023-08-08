a47a71d1fbdcb7c999aea146dcd60b5a1dff4583
Harrel <marcin.slowik95@gmail.com>
2023-08-08 23:06:50 +0200

    feat: too many implementations - scrollbar for case table

--- frontend/src/components/Cases/CaseItem.jsx
+++ frontend/src/components/Cases/CaseItem.jsx
@@ -10,42 +10,44 @@ const CaseContent = ({ seq, caseData, implementations }) => {
   return (
   <>
     <SchemaDisplay schema={caseData.schema} instance={instance} />
-    <table className="table table-hover">
-      <thead>
-      <tr>
-        <td scope="col">
-          <b>Tests</b>
-        </td>
-        {implementations.map((implementation) => (
-          <td
-            className="text-center"
-            scope="col"
-            key={
-              implementation.metadata.name +
-              implementation.metadata.language
-            }
-          >
-            <b>{implementation.metadata.name + " "}</b>
-            <small className="text-muted">
-              {implementation.metadata.language}
-            </small>
+    <div className='overflow-x-auto'>
+      <table className="table table-hover">
+        <thead>
+        <tr>
+          <td scope="col">
+            <b>Tests</b>
           </td>
-        ))}
-      </tr>
-      </thead>
-      <tbody>
-      {caseData.tests.map((test, index) => (
-        <tr key={index} onClick={() => setInstance(test.instance)}>
-          <td>
-            <p className="m-0">{test.description}</p>
-          </td>
-          {implementations.map((impl, i) => (
-            <CaseResultSvg key={i} result={impl.cases.get(seq)[index]} />
+          {implementations.map((implementation) => (
+            <td
+              className="text-center"
+              scope="col"
+              key={
+                implementation.metadata.name +
+                implementation.metadata.language
+              }
+            >
+              <b>{implementation.metadata.name + " "}</b>
+              <small className="text-muted">
+                {implementation.metadata.language}
+              </small>
+            </td>
           ))}
         </tr>
-      ))}
-      </tbody>
-    </table>
+        </thead>
+        <tbody>
+        {caseData.tests.map((test, index) => (
+          <tr key={index} onClick={() => setInstance(test.instance)}>
+            <td>
+              <p className="m-0">{test.description}</p>
+            </td>
+            {implementations.map((impl, i) => (
+              <CaseResultSvg key={i} result={impl.cases.get(seq)[index]} />
+            ))}
+          </tr>
+        ))}
+        </tbody>
+      </table>
+    </div>
   </>
 )
 }
