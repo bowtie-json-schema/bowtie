00799e1c6c2e55da1eab04c4bb22e4dafaf35ff2
Harrel <marcin.slowik95@gmail.com>
2023-08-09 22:29:15 +0200

    add missing file

---
+++ frontend/src/data/mapLanguage.ts
@@ -0,0 +1,16 @@
+const langMap: Record<string, string> = {
+  clojure: "Clojure",
+  "c++": "C++",
+  dotnet: ".NET",
+  go: "Go",
+  java: "Java",
+  javascript: "JavaScript",
+  kotlin: "Kotlin",
+  lua: "Lua",
+  python: "Python",
+  ruby: "Ruby",
+  rust: "Rust",
+  typescript: "TypeScript",
+}
+
+export const mapLanguage = (lang: string) => langMap[lang] ?? lang;
