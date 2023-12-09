042887fdb462ff5e5926e8e5a1451d301b3604e0
Julian Berman <Julian@GrayVines.com>
2023-12-09 11:44:58 -0500

    Default to title casing for our prettified language name in the UI.

--- frontend/src/data/mapLanguage.ts
+++ frontend/src/data/mapLanguage.ts
@@ -1,17 +1,9 @@
 const langMap: Record<string, string> = {
-  clojure: "Clojure",
-  "c++": "C++",
   dotnet: ".NET",
-  go: "Go",
-  java: "Java",
   javascript: "JavaScript",
-  kotlin: "Kotlin",
-  lua: "Lua",
   php: "PHP",
-  python: "Python",
-  ruby: "Ruby",
-  rust: "Rust",
   typescript: "TypeScript",
 };
 
-export const mapLanguage = (lang: string) => langMap[lang] ?? lang;
+export const mapLanguage = (lang: string) =>
+  langMap[lang] ?? lang.charAt(0).toUpperCase().concat(lang.substring(1));
