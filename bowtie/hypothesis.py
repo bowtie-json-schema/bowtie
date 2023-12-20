b5b3bd9f28cc46aa511eb9009e56ae0a3349c73f
Julian Berman <Julian@GrayVines.com>
2023-12-20 11:52:31 -0500

    Add strategies for some result types.

--- bowtie/hypothesis.py
+++ bowtie/hypothesis.py
@@ -10,14 +10,19 @@ from string import printable
 from hypothesis.strategies import (
     booleans,
     builds,
+    composite,
     dictionaries,
     floats,
     integers,
+    just,
     lists,
     none,
     recursive,
     register_type_strategy,
+    sampled_from,
+    sets,
     text,
+    tuples,
 )
 
 from bowtie import _commands
@@ -31,6 +36,9 @@ schemas = booleans() | dictionaries(
         | dictionaries(text(printable), children),
     ),
 )
+
+seq = integers(min_value=1)
+implementations = text(printable, min_size=1)
 
 
 def tests(
@@ -67,8 +75,74 @@ def test_cases(
     )
 
 
+test_results = sampled_from(
+    [
+        _commands.TestResult.VALID,
+        _commands.TestResult.INVALID,
+    ],
+)
+
+
+@composite
+def case_results(
+    draw,
+    implementations=implementations,
+    seq=seq,
+    test_results=test_results,
+    expected=booleans() | none(),
+    min_tests=1,
+    max_tests=20,
+):
+    """
+    A result for an individual test case.
+
+    Note that the `Seq` for this result will be arbitrary, so don't use this to
+    generate results in isolation without controlling for these IDs being what
+    they need to be (i.e. report-wide unique).
+    """
+    size = draw(integers(min_value=min_tests, max_value=max_tests))
+    # FIXME: only successful here
+    return _commands.CaseResult(
+        implementation=draw(implementations),
+        seq=draw(seq),
+        results=draw(lists(test_results, min_size=size, max_size=size)),
+        expected=draw(lists(expected, min_size=size, max_size=size)),
+    )
+
+
+@composite
+def cases_and_results(
+    draw,
+    implementations=sets(implementations, min_size=1),
+    test_cases=test_cases(),
+    min_cases=1,
+    max_cases=10,
+):
+    """
+    A set of test cases along with their results for generated implementations.
+    """
+    impls = draw(implementations)
+
+    strategy = lists(
+        tuples(seq, test_cases),
+        min_size=min_cases,
+        max_size=max_cases,
+        unique_by=lambda each: each[0],
+    )
+    seq_cases = draw(strategy)
+
+    return seq_cases, [
+        draw(case_results(seq=just(seq), implementations=just(implementation)))
+        for implementation in impls
+        for (seq, _) in seq_cases
+    ]
+
+
 # FIXME: These don't seem to do anything (in that builds() still fails?)
 #        I also don't really understand why the builtin attrs support doesn't
 #        autodetect more than it seems to be.
+register_type_strategy(_commands.CaseResult, case_results())
+register_type_strategy(_commands.Seq, seq)
 register_type_strategy(_commands.Test, tests())
 register_type_strategy(_commands.TestCase, test_cases())
+register_type_strategy(_commands.TestResult, test_results)
--- tests/test_hypothesis.py
+++ tests/test_hypothesis.py
@@ -17,3 +17,17 @@ def test_tests_valid(test):
 @given(bowtie.hypothesis.tests(valid=just(False)))
 def test_tests_invalid(test):
     assert not test.valid
+
+
+@given(bowtie.hypothesis.case_results())
+def test_case_results_have_at_least_one_test(case_result):
+    assert len(case_result.results) >= 1
+
+
+@given(
+    bowtie.hypothesis.cases_and_results(implementations=just({"foo", "bar"})),
+)
+def test_cases_and_results_with_given_implementations(seq_cases_results):
+    seq_cases, results = seq_cases_results
+    # all implementations have results
+    assert len(results) == len({"foo", "bar"}) * len(seq_cases)
