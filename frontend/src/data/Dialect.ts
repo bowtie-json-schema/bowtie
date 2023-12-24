adecf19562fa94b32356f4bac5caae3af94acd52
Immortal <sanskarsoni300@gmail.com>
2023-12-24 07:03:17 +0530

    Added firstPublicationDate in Dialects

--- frontend/src/data/Dialect.ts
+++ frontend/src/data/Dialect.ts
@@ -9,10 +9,16 @@ export default class Dialect {
   readonly path: string;
   readonly prettyName: string;
   readonly uri: string;
+  readonly firstPublicationDate: Date;
 
   private static all: Map<string, Dialect> = new Map<string, Dialect>();
 
-  constructor(path: string, prettyName: string, uri: string) {
+  constructor(
+    path: string,
+    prettyName: string,
+    uri: string,
+    firstPublicationDate: string,
+  ) {
     if (Dialect.all.has(path)) {
       throw new DialectError(`A "${path}" dialect already exists.`);
     }
@@ -21,6 +27,7 @@ export default class Dialect {
     this.path = path;
     this.prettyName = prettyName;
     this.uri = uri;
+    this.firstPublicationDate = new Date(firstPublicationDate);
   }
 
   async fetchReport(baseURI: URI) {
@@ -59,29 +66,35 @@ export const DRAFT202012 = new Dialect(
   "draft2020-12",
   "Draft 2020-12",
   "https://json-schema.org/draft/2020-12/schema",
+  "January 28, 2020",
 );
 export const DRAFT201909 = new Dialect(
   "draft2019-09",
   "Draft 2019-09",
   "https://json-schema.org/draft/2019-09/schema",
+  "September 17, 2019",
 );
 export const DRAFT7 = new Dialect(
   "draft7",
   "Draft 7",
   "http://json-schema.org/draft-07/schema#",
+  "November 19, 2017",
 );
 export const DRAFT6 = new Dialect(
   "draft6",
   "Draft 6",
   "http://json-schema.org/draft-06/schema#",
+  "April 21, 2017",
 );
 export const DRAFT4 = new Dialect(
   "draft4",
   "Draft 4",
   "http://json-schema.org/draft-04/schema#",
+  "January 31, 2013",
 );
 export const DRAFT3 = new Dialect(
   "draft3",
   "Draft 3",
   "http://json-schema.org/draft-03/schema#",
+  "November 22, 2010",
 );
