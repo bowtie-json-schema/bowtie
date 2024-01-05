a4f651e3eb1c09075fe184524941c0dcae507773
Julian Berman <Julian@GrayVines.com>
2024-01-05 08:37:25 -0500

    vite/jest configs -> ts

---
+++ frontend/jest.config.ts
@@ -0,0 +1,8 @@
+import type { JestConfigWithTsJest } from "ts-jest";
+
+const jestConfig: JestConfigWithTsJest = {
+  transform: {
+    "^.+\\.tsx?$": ["ts-jest", {}],
+  },
+};
+export default jestConfig;
