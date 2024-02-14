69eca511960aee7cf182ea817a1e5dcfb12be225
jarvis <dhruv1778@gmail.com>
2024-02-14 17:20:26 +0530

    Change tooltip trigger for new implementation

--- frontend/src/components/Summary/ImplementationRow.tsx
+++ frontend/src/components/Summary/ImplementationRow.tsx
@@ -4,7 +4,6 @@ import { DetailsButtonModal } from "../Modals/DetailsButtonModal";
 import { mapLanguage } from "../../data/mapLanguage";
 import { NavLink, useNavigate } from "react-router-dom";
 import { Case, ImplementationData } from "../../data/parseReportData";
-import { Plus } from "react-bootstrap-icons";
 import { OverlayTrigger, Tooltip } from "react-bootstrap";
 const ImplementationRow = ({
   cases,
@@ -20,84 +19,81 @@ const ImplementationRow = ({
   const implementationPath = getImplementationPath(implementation);
 
   return (
-    <tr className={implementation.isNew ? "table-success" : ""}>
-      <th
-        className="table-implementation-name"
-        onClick={() => navigate(`/implementations/${implementationPath}`)}
-        scope="row"
-      >
-        <NavLink
-          className={implementation.isNew ? "text-primary" : ""}
-          to={`/implementations/${implementationPath}`}
+    <OverlayTrigger
+      placement="right"
+      overlay={
+        <Tooltip className={implementation.isNew ? "" : "d-none"} id="tooltip">
+          <strong>Newly added implementation</strong>
+        </Tooltip>
+      }
+    >
+      <tr className={implementation.isNew ? "table-success" : ""}>
+        <th
+          className="table-implementation-name"
+          onClick={() => navigate(`/implementations/${implementationPath}`)}
+          scope="row"
         >
-          {implementation.metadata.name}
-        </NavLink>
-        <small
-          className={
-            "ps-1 " + (implementation.isNew ? "text-dark" : "text-muted")
-          }
-        >
-          {mapLanguage(implementation.metadata.language)}
-        </small>
-      </th>
-      <td>
-        <small
-          className={
-            "font-monospace " +
-            (implementation.isNew ? "text-dark" : "text-muted")
-          }
-        >
-          {implementation.metadata.version ?? ""}
-        </small>
-      </td>
-
-      <td className="text-center">{implementation.erroredCases}</td>
-      <td className="text-center">{implementation.skippedTests}</td>
-      <td className="text-center details-required">
-        {implementation.failedTests + implementation.erroredTests}
-        <div className="hover-details text-center">
-          <p>
-            <b>failed</b>:{implementation.failedTests}
-          </p>
-          <p>
-            <b>errored</b>:{implementation.erroredTests}
-          </p>
-        </div>
-      </td>
-
-      <td>
-        {implementation.failedTests +
-          implementation.erroredTests +
-          implementation.skippedTests >
-          0 && (
-          <button
-            type="button"
-            className="btn btn-sm btn-primary"
-            onClick={() => setShowDetails(true)}
+          <NavLink
+            className={implementation.isNew ? "text-primary" : ""}
+            to={`/implementations/${implementationPath}`}
           >
-            Details
-          </button>
-        )}
-        {implementation.isNew && (
-          <OverlayTrigger
-            placement="right"
-            overlay={
-              <Tooltip id="tooltip">
-                <strong>Newly added implementation</strong>
-              </Tooltip>
+            {implementation.metadata.name}
+          </NavLink>
+          <small
+            className={
+              "ps-1 " + (implementation.isNew ? "text-dark" : "text-muted")
             }
           >
-            <Plus />
-          </OverlayTrigger>
-        )}
-      </td>
-      <DetailsButtonModal
-        show={showDetails}
-        handleClose={() => setShowDetails(false)}
-        cases={cases}
-        implementation={implementation}
-      />
-    </tr>
+            {mapLanguage(implementation.metadata.language)}
+          </small>
+        </th>
+        <td>
+          <small
+            className={
+              "font-monospace " +
+              (implementation.isNew ? "text-dark" : "text-muted")
+            }
+          >
+            {implementation.metadata.version ?? ""}
+          </small>
+        </td>
+
+        <td className="text-center">{implementation.erroredCases}</td>
+        <td className="text-center">{implementation.skippedTests}</td>
+        <td className="text-center details-required">
+          {implementation.failedTests + implementation.erroredTests}
+          <div className="hover-details text-center">
+            <p>
+              <b>failed</b>:{implementation.failedTests}
+            </p>
+            <p>
+              <b>errored</b>:{implementation.erroredTests}
+            </p>
+          </div>
+        </td>
+
+        <td>
+          {implementation.failedTests +
+            implementation.erroredTests +
+            implementation.skippedTests >
+            0 && (
+              <button
+                type="button"
+                className="btn btn-sm btn-primary"
+                onClick={() => setShowDetails(true)}
+              >
+                Details
+              </button>
+            )}
+        </td>
+        <DetailsButtonModal
+          show={showDetails}
+          handleClose={() => setShowDetails(false)}
+          cases={cases}
+          implementation={implementation}
+        />
+      </tr>
+    </OverlayTrigger>
   );
 };
 
