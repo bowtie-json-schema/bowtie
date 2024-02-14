8c1a2b260912206006a7f63d682ec6ffc31a27ca
Adwait <adwaitngodbole@gmail.com>
2024-02-15 03:10:47 +0530

    Locale Date Formatting
    Formatting date locally based on host environment's locale settings.

--- frontend/src/utils/formatDate.ts
+++ frontend/src/utils/formatDate.ts
@@ -1,10 +1,11 @@
 export const formatDate = (date: string) => {
   const dateObj = new Date(date);
   const options: Intl.DateTimeFormatOptions = {
+    weekday: "long",
     year: "numeric",
     month: "long",
     day: "numeric",
   };
-  const formattedDate = dateObj.toLocaleDateString("en-US", options);
+  const formattedDate = dateObj.toLocaleDateString(undefined, options);
   return formattedDate;
 };
