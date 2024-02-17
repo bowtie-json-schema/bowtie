cce3520d024ff41a8a701e0e5eef741371100798
Julian Berman <Julian@GrayVines.com>
2024-02-17 13:44:44 -0500

    Reformat the about section.
    No need for a table, it's just some text.
    
    And remove the extra non-breaking spaces.

--- frontend/src/components/BowtieInfo/BowtieInfoSection.tsx
+++ frontend/src/components/BowtieInfo/BowtieInfoSection.tsx
@@ -1,34 +1,19 @@
 const BowtieInfoSection = () => {
+  // prettier-ignore
   return (
     <div className="card mx-auto mb-3 w-75" id="bowtie-info">
       <div className="card-header">About Bowtie</div>
       <div className="card-body">
-        <table className="table table-sm table-hover">
-          <thead>
-            <tr>
-              <td>
-                Bowtie is tool for understanding and comparing implementations
-                of the &nbsp;
-                <a href={"https://json-schema.org/"}>
-                  {"JSON Schema specification"}
-                </a>
-                &nbsp; across all programming languages. The report below uses
-                the &nbsp;
-                <a
-                  href={
-                    "https://github.com/json-schema-org/JSON-Schema-Test-Suite"
-                  }
-                >
-                  {"official JSON Schema Test Suite"}
-                </a>
-                &nbsp; to display bugs or functionality gaps in implementations.
-                You can also use Bowtie to get this information about your own
-                schemas. You can find out how in its &nbsp;
-                <a href={"https://docs.bowtie.report/"}>{"documentation"}</a>.
-              </td>
-            </tr>
-          </thead>
-        </table>
+        <p>
+          Bowtie is tool for understanding and comparing implementations of the <a href={"https://json-schema.org/"}>{"JSON Schema specification"}</a> across all programming languages.
+        </p>
+        <p>
+          The report below uses the <a href={"https://github.com/json-schema-org/JSON-Schema-Test-Suite"}>{"official JSON Schema Test Suite"}</a> to display bugs or functionality gaps in implementations.
+        </p>
+        <p>
+          You can also use Bowtie to get this information about your own schemas.
+          Find out how in <a href={"https://docs.bowtie.report/"}>{"Bowtie's documentation"}</a>.
+        </p>
       </div>
     </div>
   );
