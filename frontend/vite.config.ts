72f5d73e249deabf148589e30b37aa9e9c0b2966
Kushagra <skushagra.sharma@gmail.com>
2024-02-23 15:18:23 +0530

    added shadcn

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path"
 
 export default defineConfig({
   plugins: [react()],
@@ -7,5 +8,10 @@ export default defineConfig({
     sourcemap: true,
     target: ["es2022"],
     outDir: "build",
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
   },
 });
