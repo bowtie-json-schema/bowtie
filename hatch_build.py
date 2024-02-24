731ee976117765bca2acc84fc7563739ff50f919
Adwait <adwaitngodbole@gmail.com>
2024-02-24 20:38:06 +0530

    docstring and line too long fixes

--- hatch_build.py
+++ hatch_build.py
@@ -1,23 +1,40 @@
-from hatchling.builders.hooks.plugin.interface import BuildHookInterface
+"""
+Responsible for defining the custom build hook plugin class.
+"""
+from pathlib import Path
 from typing import Any
 import os
 
+from hatchling.builders.hooks.plugin.interface import BuildHookInterface
+
+
 class CustomBuildHook(BuildHookInterface):
+    """
+    A custom build hook plugin class.
+
+    Subclasses the BuildHookInterface to let know hatchling
+    about a custom build hook plugin and defines the
+    initialize method.
+    """
+
     def initialize(
-        self, 
-        version: str, 
-        build_data: dict[str, Any]
+        self,
+        version: str,
+        build_data: dict[str, Any],
     ) -> None:
         """
-        Reads the JSON file path from environment variable and adds it to shared data.
+        Includes the json file in the final build.
+
+        Reads the JSON file path from environment variable and
+        force includes it in the final build.
         """
         if not os.environ.get("RUNNING_HATCH_SESSION"):
             return
         json_file_path = os.environ.get("IMPLEMENTATIONS_METADATA_FILE_PATH")
 
-        if not json_file_path or not os.path.exists(json_file_path):
+        if not json_file_path or not Path(json_file_path).exists():
             raise RuntimeError(
-                "Generated JSON file not found"
+                "Generated JSON file not found",
             )
 
-        build_data['force_include']['implementations.json'] = json_file_path
+        build_data["force_include"]["implementations.json"] = json_file_path
