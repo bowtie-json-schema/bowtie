674954896ae9accd1341f675fbfecd4f15f57f91
Adwait <adwaitngodbole@gmail.com>
2024-02-24 19:56:43 +0530

    maybe setting the env variable works

--- hatch_build.py
+++ hatch_build.py
@@ -11,6 +11,8 @@ class CustomBuildHook(BuildHookInterface):
         """
         Reads the JSON file path from environment variable and adds it to shared data.
         """
+        if not os.environ.get("RUNNING_HATCH_SESSION"):
+            return
         json_file_path = os.environ.get("IMPLEMENTATIONS_METADATA_FILE_PATH")
 
         if not json_file_path or not os.path.exists(json_file_path):
@@ -18,5 +20,4 @@ class CustomBuildHook(BuildHookInterface):
                 "Generated JSON file not found"
             )
 
-        # Add file to shared data
         build_data['force_include']['implementations.json'] = json_file_path
