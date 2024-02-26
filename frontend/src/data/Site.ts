aed446be8ada0c6f36cf646af94168872f4ec6dd
Julian Berman <Julian@GrayVines.com>
2024-02-26 17:44:05 -0500

    Erm, explicitly strip the fragment.
    It appears present in the prod UI, which comes from window.location.href I guess.

--- frontend/src/data/Site.ts
+++ frontend/src/data/Site.ts
@@ -5,7 +5,9 @@ const base =
     ? "https://bowtie.report"
     : window.location.href;
 
-export const siteURI = new URI(base).directory(import.meta.env.BASE_URL);
+export const siteURI = new URI(base)
+  .fragment("")
+  .directory(import.meta.env.BASE_URL);
 
 // FIXME: Presumably a future `Implementation` can handle the below, just
 //        as `Dialect` handles it for dialect reports.
