e4f72f1ad3c46a371afd72615ccecabef49bda57
Swarnendu <94161758+Swarnendu0123@users.noreply.github.com>
2024-02-28 22:56:02 +0530

    Update Site.ts

--- frontend/src/data/Site.ts
+++ frontend/src/data/Site.ts
@@ -1,15 +1,19 @@
+import URI from "urijs";
+
 const base =
   import.meta.env.MODE === "development"
     ? "https://bowtie.report"
     : window.location.href;
 
-export const siteURL = new URL(base);
-siteURL.hash = "";
-siteURL.pathname = import.meta.env.BASE_URL;
+export const siteURI = new URI(base)
+  .fragment("")
+  .directory(import.meta.env.BASE_URL);
 
-export const implementationMetadataURL: string = new URL(
-  "implementations.json",
-  siteURL,
-).href;
+// FIXME: Presumably a future `Implementation` can handle the below, just
+//        as `Dialect` handles it for dialect reports.
+export const implementationMetadataURI = siteURI
+  .clone()
+  .filename("implementations.json")
+  .href();
 
-export default siteURL;
+export default siteURI;
