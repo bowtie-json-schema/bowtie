30955b8c46b3bb953ff63f20b412db48af2a3e0e
Adwait <adwaitngodbole@gmail.com>
2024-03-12 01:58:01 +0530

    fixed badge tests

--- frontend/src/data/Badge.test.ts
+++ frontend/src/data/Badge.test.ts
@@ -15,21 +15,19 @@ const mockImplementation: Implementation = {
 
 describe("Badge", () => {
   test("versionsBadgeFor should return the correct URL", () => {
-    const expectedUrl = new URL(
-      "https://img.shields.io/endpoint?url=https://bowtie.report/badges/javascript-node/supported_versions.json",
-    );
     const badgeUrl = versionsBadgeFor(mockImplementation);
-    expect(badgeUrl.href).toBe(expectedUrl.href);
+    expect(badgeUrl.href).toEqual(
+      "https://img.shields.io/endpoint?url=http%3A%2F%2Flocalhost%3A3000%2F%2Fbadges%2Fjavascript-node%2Fsupported_versions.json"
+    );
   });
 
   test("complianceBadgeFor should return the correct URL", () => {
-    const expectedUrl = new URL(
-      "https://img.shields.io/endpoint?url=https://bowtie.report/badges/javascript-node/compliance/draft7.json",
-    );
     const badgeURL = complianceBadgeFor(
       mockImplementation,
-      Dialect.withName("draft7"),
+      Dialect.withName("draft7")
     );
-    expect(badgeURL.href).toBe(expectedUrl.href);
+    expect(badgeURL.href).toEqual(
+      "https://img.shields.io/endpoint?url=http%3A%2F%2Flocalhost%3A3000%2F%2Fbadges%2Fjavascript-node%2Fcompliance%2Fdraft7.json"
+    );
   });
 });
