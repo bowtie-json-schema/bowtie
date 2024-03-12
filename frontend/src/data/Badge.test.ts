2f764fcf184f3975901414235b0622431c0f2d52
Adwait <adwaitngodbole@gmail.com>
2024-03-12 14:35:13 +0530

    Update Badge.test.ts

--- frontend/src/data/Badge.test.ts
+++ frontend/src/data/Badge.test.ts
@@ -1,4 +1,4 @@
-import { describe, expect, test, vi, beforeEach } from "vitest";
+import { describe, expect, test } from "vitest";
 import Dialect from "./Dialect";
 import { complianceBadgeFor, versionsBadgeFor } from "./Badge";
 import { Implementation } from "./parseReportData";
@@ -14,36 +14,20 @@ const mockImplementation: Implementation = {
 };
 
 describe("Badge", () => {
-  beforeEach(() => {
-    vi.unstubAllGlobals();
-  });
-
   test("versionsBadgeFor should return the correct URL", () => {
-    vi.stubGlobal("window", {
-      location: {
-        href: "http://localhost:8000",
-      },
-    });
-
     const badgeUrl = versionsBadgeFor(mockImplementation);
     expect(badgeUrl.href()).toEqual(
-      "https://img.shields.io/endpoint?url=http%3A%2F%2Flocalhost%3A8000%2Fbadges%2Fjavascript-node%2Fsupported_versions.json",
+      "https://img.shields.io/endpoint?url=http%3A%2F%2Flocalhost%3A3000%2Fbadges%2Fjavascript-node%2Fsupported_versions.json"
     );
   });
 
   test("complianceBadgeFor should return the correct URL", () => {
-    vi.stubGlobal("window", {
-      location: {
-        href: "http://localhost:8000",
-      },
-    });
-
     const badgeURL = complianceBadgeFor(
       mockImplementation,
-      Dialect.withName("draft7"),
+      Dialect.withName("draft7")
     );
     expect(badgeURL.href()).toEqual(
-      "https://img.shields.io/endpoint?url=http%3A%2F%2Flocalhost%3A8000%2Fbadges%2Fjavascript-node%2Fcompliance%2Fdraft7.json",
+      "https://img.shields.io/endpoint?url=http%3A%2F%2Flocalhost%3A3000%2Fbadges%2Fjavascript-node%2Fcompliance%2Fdraft7.json"
     );
   });
 });
