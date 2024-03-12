09b149aca9ecea156f402606f2d9c76dea253c96
Adwait <adwaitngodbole@gmail.com>
2024-03-13 01:49:31 +0530

    Update core.clj

--- implementations/clojure-json-schema/src/bowtie_json_schema/core.clj
+++ implementations/clojure-json-schema/src/bowtie_json_schema/core.clj
@@ -25,11 +25,7 @@
                                      "http://json-schema.org/draft-04/schema#"]
                           :os (System/getProperty "os.name")
                           :os_version (System/getProperty "os.version")
-                          :language_version (str (:major clojure-version)
-                                                 "."
-                                                 (:minor clojure-version)
-                                                 "."
-                                                 (:incremental clojure-version))}})
+                          :language_version (str (:version clojure-version))}})
             "dialect" (do (assert @started "Not started!")
                           {:ok false})
             "run" (do (assert @started "Not started!")
