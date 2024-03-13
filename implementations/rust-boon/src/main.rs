2f7ccc4ab6585cf299e3e39c7a0b84d209fe2b1e
Adwait <adwaitngodbole@gmail.com>
2024-03-13 23:43:11 +0530

    Update main.rs

--- implementations/rust-boon/src/main.rs
+++ implementations/rust-boon/src/main.rs
@@ -33,7 +33,7 @@ fn main() -> Result<(), Box<dyn Error>> {
                             "http://json-schema.org/draft-04/schema#",
                         ],
                         "os": os_info::get().os_type(),
-                        "os_version": os_info::get().version(),
+                        "os_version": os_info::get().version().to_string(),
                         "language_version": rustc_version_runtime::version().to_string(),
                     }
                 });
