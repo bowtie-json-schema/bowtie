4decda589f5f9ff082e74425488ab8549d7b9a82
Julian Berman <Julian@GrayVines.com>
2024-03-19 16:13:14 -0400

    Minor badge page tweaks.
    Still plenty of issues left.

--- frontend/src/components/ImplementationReportView/EmbedBadges.tsx
+++ frontend/src/components/ImplementationReportView/EmbedBadges.tsx
@@ -47,121 +47,116 @@ const EmbedBadges: React.FC<{
       >
         Badges
       </button>
-      <ul className="dropdown-menu mx-auto mb-3 col-12 col-md-11 col-lg-8 col-xxl-7">
-        <li>
-          <div className="px-4 pt-1">
-            <h4 className="ps-sm-1 text-nowrap text-center d-flex justify-content-center justify-content-lg-start">
-              Embed a Badge
-            </h4>
-            <div className="dropdown d-lg-flex justify-content-center justify-content-sm-start pt-2 text-center">
-              <label
-                className="pb-1 pe-3 ps-1  text-nowrap "
-                htmlFor="dropdownMenuButton"
-              >
-                <small>Available Badges:</small>
-              </label>
-              <button
-                className="btn btn-sm btn-primary dropdown-toggle mw-100 overflow-hidden"
-                type="button"
-                id="dropdownMenuButton"
-                data-bs-toggle="dropdown"
-                aria-expanded="false"
-              >
-                {altTextFor(activeBadge)}
-              </button>
-              <ul
-                className="dropdown-menu"
-                aria-labelledby="dropdownMenuButton"
-              >
-                <h6 className="dropdown-header">Supported Dialects</h6>
-                <li key={"JSON Schema Versions"}>
+      <div className="dropdown-menu mx-auto">
+        <div className="px-4 pt-1">
+          <h4 className="ps-sm-1 text-nowrap text-center d-flex justify-content-center justify-content-lg-start">
+            Embed a Badge
+          </h4>
+          <div className="dropdown d-lg-flex justify-content-center justify-content-sm-start pt-2 text-center">
+            <label
+              className="pb-1 pe-3 ps-1  text-nowrap "
+              htmlFor="dropdownMenuButton"
+            >
+              Available Badges
+            </label>
+            <button
+              className="btn btn-sm btn-primary dropdown-toggle mw-100 overflow-hidden"
+              type="button"
+              id="dropdownMenuButton"
+              data-bs-toggle="dropdown"
+              aria-expanded="false"
+            >
+              {altTextFor(activeBadge)}
+            </button>
+            <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton">
+              <h6 className="dropdown-header">Implementation Metadata</h6>
+              <li key={"JSON Schema Versions"}>
+                <button
+                  className={`dropdown-item btn btn-sm ${
+                    activeBadge === "JSON Schema Versions" ? "active" : ""
+                  }`}
+                  onClick={() =>
+                    handleSelectBadge(
+                      "JSON Schema Versions",
+                      versionsBadgeFor(implementation),
+                    )
+                  }
+                >
+                  {"JSON Schema Versions"}
+                </button>
+              </li>
+              <h6 className="dropdown-header">Specification Compliance</h6>
+              {results.map((result) => (
+                <li key={result[0]}>
                   <button
                     className={`dropdown-item btn btn-sm ${
-                      activeBadge === "JSON Schema Versions" ? "active" : ""
+                      result[0] === activeBadge ? "active" : ""
                     }`}
                     onClick={() =>
                       handleSelectBadge(
-                        "JSON Schema Versions",
-                        versionsBadgeFor(implementation),
+                        result[0],
+                        complianceBadgeFor(
+                          implementation,
+                          Dialect.withName(result[0]),
+                        ),
                       )
                     }
                   >
-                    {"JSON Schema Versions"}
+                    {Dialect.withName(result[0]).prettyName}
                   </button>
                 </li>
-                <h6 className="dropdown-header">Compliance Badges</h6>
-                {results.map((result) => (
-                  <li key={result[0]}>
-                    <button
-                      className={`dropdown-item btn btn-sm ${
-                        result[0] === activeBadge ? "active" : ""
-                      }`}
-                      onClick={() =>
-                        handleSelectBadge(
-                          result[0],
-                          complianceBadgeFor(
-                            implementation,
-                            Dialect.withName(result[0]),
-                          ),
-                        )
-                      }
-                    >
-                      {Dialect.withName(result[0]).prettyName}
-                    </button>
-                  </li>
-                ))}
-              </ul>
-            </div>
-          </div>
-          <div className="container vw-50 d-flex justify-content-center align-items-center flex-column pt-3">
-            <ul className="nav nav-pills justify-content-center">
-              {supportedFormats.map((formatItem, index) => {
-                return (
-                  <li className="nav-item" key={index}>
-                    <button
-                      className={`nav-link btn btn-sm ${
-                        activeTab === formatItem.name ? "active" : ""
-                      }`}
-                      onClick={() => handleSelectTab(formatItem.name)}
-                    >
-                      {formatItem.name}
-                    </button>
-                  </li>
-                );
-              })}
+              ))}
             </ul>
-            <div className="tab-content mt-2 pt-2 pb-2 w-100">
-              {supportedFormats.map((formatItem, index) => {
-                const badgeEmbed = formatItem.generateEmbed(
-                  badgeURI.href(),
-                  altTextFor(activeBadge),
-                );
-                return (
-                  <div
-                    key={index}
-                    className={`tab-pane ${
+          </div>
+        </div>
+        <div className="container vw-50 d-flex justify-content-center align-items-center flex-column pt-3">
+          <ul className="nav nav-pills justify-content-center">
+            {supportedFormats.map((formatItem, index) => {
+              return (
+                <li className="nav-item" key={index}>
+                  <button
+                    className={`nav-link btn btn-sm ${
                       activeTab === formatItem.name ? "active" : ""
-                    } border rounded pt-1 px-4 mx-2`}
+                    }`}
+                    onClick={() => handleSelectTab(formatItem.name)}
                   >
-                    <div className="d-flex align-items-center justify-content-center px-1">
-                      <div className="w-100">
-                        <span className="font-monospace text-body-secondary fs-6 ps-2 d-block">
-                          <pre className="pt-2 pb-2">
-                            <code>{badgeEmbed}</code>
-                          </pre>
-                        </span>
-                      </div>
-                      <div className="ms-auto pb-2 px-1">
-                        <CopyToClipboard textToCopy={badgeEmbed} />
-                      </div>
+                    {formatItem.name}
+                  </button>
+                </li>
+              );
+            })}
+          </ul>
+          <div className="tab-content mt-2 pt-2 pb-2 w-100">
+            {supportedFormats.map((formatItem, index) => {
+              const badgeEmbed = formatItem.generateEmbed(
+                badgeURI.href(),
+                altTextFor(activeBadge),
+              );
+              return (
+                <div
+                  key={index}
+                  className={`tab-pane ${
+                    activeTab === formatItem.name ? "active" : ""
+                  } border rounded pt-1 px-4 mx-2`}
+                >
+                  <div className="d-flex align-items-center justify-content-center px-1">
+                    <div className="w-100">
+                      <span className="font-monospace text-body-secondary fs-6 ps-2 d-block">
+                        <pre className="pt-2 pb-2">
+                          <code>{badgeEmbed}</code>
+                        </pre>
+                      </span>
+                    </div>
+                    <div className="ms-auto pb-2 px-1">
+                      <CopyToClipboard textToCopy={badgeEmbed} />
                     </div>
                   </div>
-                );
-              })}
-            </div>
+                </div>
+              );
+            })}
           </div>
-        </li>
-      </ul>
+        </div>
+      </div>
     </div>
   );
 };
