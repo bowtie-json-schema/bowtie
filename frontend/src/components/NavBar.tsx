4cdf48eb5a1112eff0cc8f637f9751e1223b08d8
Sudip Mandal <skmxyt@gmail.com>
2024-03-29 14:41:07 +0530

    Remove unused props from hamburger button

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -59,8 +59,6 @@ const NavBar = () => {
               className="navbar-toggler"
               type="button"
               onClick={() => setIsNavbarOpen(!isNavbarOpen)}
-              data-bs-toggle="collapse"
-              data-bs-target="#navbarNav"
               aria-controls="navbarSupportedContent"
               aria-expanded="false"
               aria-label="Toggle navigation"
