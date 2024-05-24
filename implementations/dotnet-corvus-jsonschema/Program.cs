386c67be427990ca1ea150b7721f694a145f4077
Adwait <adwaitngodbole@gmail.com>
2024-05-24 12:56:56 +0530

    syntax error fix

--- implementations/dotnet-corvus-jsonschema/Program.cs
+++ implementations/dotnet-corvus-jsonschema/Program.cs
@@ -151,7 +151,6 @@ while (cmdSource.GetNextCommand() is {} line && line != string.Empty)
                         ["os"] = Environment.OSVersion.Platform.ToString(),
                         ["os_version"] = Environment.OSVersion.Version.ToString(),
                         ["language_version"] = Environment.Version.ToString() },
-                    },
             };
             Console.WriteLine(startResult.ToJsonString());
             break;
