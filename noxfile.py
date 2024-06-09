1fdd7e8ebee53be7347b1db484904c446ad1105b
Julian Berman <Julian@GrayVines.com>
2024-06-09 12:08:13 +0300

    Drop 3.10 from our nox/GHA bits.

--- noxfile.py
+++ noxfile.py
@@ -39,7 +39,7 @@ REQUIREMENTS_IN = [  # this is actually ordered, as files depend on each other
 ]
 
 
-SUPPORTED = ["pypy3.10", "3.11", "3.12"]
+SUPPORTED = ["3.11", "3.12"]
 LATEST = SUPPORTED[-1]
 
 nox.options.default_venv_backend = "uv|virtualenv"
