9691749cf039449612c6503850bae922a1765f3b
Adwait <adwaitngodbole@gmail.com>
2024-08-18 10:21:32 +0530

    minor refactoring

--- frontend/src/data/Implementation.ts
+++ frontend/src/data/Implementation.ts
@@ -98,8 +98,8 @@ export default class Implementation
     Object.entries(rawImplementations).forEach(([id, rawData]) =>
       parsedImplementations.set(
         id,
-        this.withId(id) ?? new Implementation(id, rawData),
-      ),
+        this.withId(id) ?? new Implementation(id, rawData)
+      )
     );
 
     return parsedImplementations;
@@ -134,16 +134,14 @@ export default class Implementation
     const versions = this.versions;
     if (!versions)
       throw new ImplementationError(
-        `No versions metadata found for ${this.id} implementation.`,
+        `No versions metadata found for ${this.id} implementation.`
       );
 
     const versionedReportsData = new Map<Dialect, Map<string, ReportData>>();
+    versionedReportsData.set(dialect, new Map<string, ReportData>());
+
     await Promise.all(
       versions.map(async (version) => {
-        if (!versionedReportsData.has(dialect)) {
-          versionedReportsData.set(dialect, new Map<string, ReportData>());
-        }
-
         try {
           const response = await fetch(
             this.directoryURI(baseURI)
@@ -151,16 +149,15 @@ export default class Implementation
               .segment(`v${version}`)
               .segment(dialect.shortName)
               .suffix("json")
-              .href(),
+              .href()
           );
-
           versionedReportsData
             .get(dialect)!
             .set(version, fromSerialized(await response.text()));
         } catch (err) {
           return;
         }
-      }),
+      })
     );
 
     return versionedReportsData;
@@ -172,7 +169,7 @@ export default class Implementation
 
   versionsBadge(): URI {
     return badgeFor(
-      this.badgesIdSegment.clone().segment("supported_versions").suffix("json"),
+      this.badgesIdSegment.clone().segment("supported_versions").suffix("json")
     );
   }
 
@@ -182,7 +179,7 @@ export default class Implementation
         .clone()
         .segment("compliance")
         .segment(dialect.shortName)
-        .suffix("json"),
+        .suffix("json")
     );
   }
 
