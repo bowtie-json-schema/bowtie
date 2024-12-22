73499f7eacf9e976ac24bdf89a1155473de21522
Julian Berman <Julian@GrayVines.com>
2024-12-22 14:53:48 -0500

    Unignore whatever pyright fixed

--- bowtie/_containers.py
+++ bowtie/_containers.py
@@ -207,7 +207,7 @@ def _float_or_none(value: str | float | None) -> float | None:
 class ConnectableImage:
 
     _id: str = field(
-        converter=lambda value: (  # type: ignore[reportUnknownArgumentType]
+        converter=lambda value: (
             value if "/" in value else f"{IMAGE_REPOSITORY}/{value}"
         ),
         alias="id",
