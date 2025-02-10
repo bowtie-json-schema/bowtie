a0251d59948e17c324f4204924c0ae5718da7dd0
Oleg Smirnov <oleg31101996@gmail.com>
2025-02-10 20:52:39 +0400

    Option method call can be chained now - remove extra compile variable

--- implementations/rust-jsonschema/src/main.rs
+++ implementations/rust-jsonschema/src/main.rs
@@ -43,8 +43,8 @@ fn main() -> Result<()> {
     ]);
 
     let mut started = false;
-    let mut options = jsonschema::options();
-    let mut compiler = options.with_draft(Draft::Draft202012);
+    let mut options = jsonschema::options()
+        .with_draft(Draft::Draft202012);
 
     for line in io::stdin().lines() {
         let request: serde_json::Value = serde_json::from_str(&line.expect("No input!"))?;
@@ -85,8 +85,8 @@ fn main() -> Result<()> {
                     panic!("Not started!")
                 };
                 let dialect = request["dialect"].as_str().expect("Bad dialect!");
-                options = jsonschema::options();
-                compiler = options.with_draft(*dialects.get(dialect).expect("No such draft!"));
+                options = jsonschema::options()
+                    .with_draft(*dialects.get(dialect).expect("No such draft!"));
                 let response = json!({"ok": true});
                 println!("{}", response);
             }
@@ -100,9 +100,9 @@ fn main() -> Result<()> {
                 let retriever = InMemoryRetriever {
                     registry: registry.to_owned(),
                 };
-                compiler = compiler.with_retriever(retriever);
+                options = options.with_retriever(retriever);
 
-                let response = match compiler.build(&case["schema"]) {
+                let response = match options.build(&case["schema"]) {
                     Ok(compiled) => {
                         let results: Vec<_> = case["tests"]
                             .as_array()
