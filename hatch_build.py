4ec0809f2e8e5f04b9fb52e98965fc9b79097dda
Oleg Smirnov <oleg31101996@gmail.com>
2025-03-04 18:17:01 +0100

    Fix styling errors

--- hatch_build.py
+++ hatch_build.py
@@ -4,13 +4,13 @@ Bowtie's Hatchling plugin, used for pregenerating implementation data.
 
 from __future__ import annotations
 
-import os
-import requests
 from pathlib import Path
 from typing import Any
 import json
+import os
 
 from hatchling.builders.hooks.plugin.interface import BuildHookInterface
+import requests
 
 
 class BowtieDataIncluder(BuildHookInterface):
@@ -25,9 +25,9 @@ class BowtieDataIncluder(BuildHookInterface):
         return Path(self.directory) / "known_implementations.json"
 
     def initialize(
-        self,
-        version: str,
-        build_data: dict[str, Any],
+            self,
+            version: str,
+            build_data: dict[str, Any],
     ) -> None:
         """
         Tell Hatchling to store data we'll need at runtime within the package.
@@ -48,39 +48,44 @@ class BowtieDataIncluder(BuildHookInterface):
         build_data["force_include"][str(out)] = target_path
 
     def finalize(
-        self,
-        version: str,
-        build_data: dict[str, Any],
-        artifact_path: str,
+            self,
+            version: str,
+            build_data: dict[str, Any],
+            artifact_path: str,
     ) -> None:
         """
         Clean up any generated temporary files.
         """
         self.known_implementations().unlink()
 
-    @staticmethod
-    def _known_implementations_from_packages_api(known_local: list[str]) -> list[str]:
+    def _known_implementations_from_packages_api(
+            self, known_local: list[str]) -> list[str]:
         """
         Collects available implementation using GitHub packages API.
         """
-        gh_token_env_var = "GITHUB_TOKEN"
-        gh_token = os.getenv(gh_token_env_var)
+        gh_token = os.getenv("GITHUB_TOKEN")
 
         if not gh_token:
-            print(f"WARN: {gh_token_env_var} env variable was not provided. "
-                  f"It will be REQUIRED in the future to collect known implementations")
+            self.app.display_warning(
+                "GITHUB_TOKEN env variable was not provided. "
+                "It will be REQUIRED in the future "
+                "to collect known implementations",
+            )
             return known_local
 
         try:
             packages = BowtieDataIncluder._collect_packages(gh_token)
         except requests.HTTPError as e:
-            print(f"ERROR: fallback to using local implementations: {e.response.text}")
+            self.app.display_error(f"fallback to using local implementations: "
+                                   f"{e.response.text}")
             return known_local
 
         packages_set = set(packages)
         local_set = set(known_local)
         if not packages_set.issuperset(local_set):
-            print(f"WARN: local implementations contain additional implementations: {local_set.difference(packages_set)}")
+            self.app.display_warning(
+                f"local implementations contain additional implementations: "
+                f"{local_set.difference(packages_set)}")
             # return local implementation until fully migrated to packages API
             # https://github.com/bowtie-json-schema/bowtie/issues/1849
             return known_local
@@ -94,6 +99,7 @@ class BowtieDataIncluder(BuildHookInterface):
         while True:
             # https://docs.github.com/en/rest/packages/packages?apiVersion=2022-11-28#list-packages-for-an-organization
             response = requests.get(
+                timeout=10,
                 url="https://api.github.com/orgs/bowtie-json-schema/packages",
                 params={
                     "package_type": "container",
@@ -102,8 +108,8 @@ class BowtieDataIncluder(BuildHookInterface):
                 headers={
                     "Accept": "application/vnd.github+json",
                     "X-GitHub-Api-Version": "2022-11-28",
-                    "Authorization": f"Bearer {gh_token}"
-                }
+                    "Authorization": f"Bearer {gh_token}",
+                },
             )
 
             with response:
